2c66e82cedaeb5797cc28576868f14426a6daee3
Sameh Mohamed Omar <sameh.mohamed@learnovia.com>
2019-07-10 08:52:05 +0000

    Media.php edited online with Bitbucket

--- Modules/UploadFiles/Entities/Media.php
+++ Modules/UploadFiles/Entities/Media.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class media extends Model
 {
     protected $fillable = ['id','name','course_segment_id','media_id'];
+    protected $hidden = ['updated_at','created_at'];
+
 }
