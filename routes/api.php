cebd2fecb48be2ab03fbc85a103deb0b3a60b5bd
bugzeenut <hossam.ahmed@learnovia.com>
2019-07-15 13:25:02 +0200

    Merge enroll to master

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin:http://localhost:4200");
+header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Methods:*");
 header("Access-Control-Allow-Headers:*");
 use App\User;
