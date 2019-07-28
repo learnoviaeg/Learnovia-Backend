c697fa67de1e00abf8a18fe2f67a012e9abc99b9
Hoda1321 <Hoda1321@bitbucket.orggit config --global user.email Hoda1321@bitbucket.orggit config --global user.email Hoda1321@bitbucket.org>
2019-07-28 16:01:52 +0200

    seed all DB

--- app/Parent.php
+++ app/Parent.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Parents extends Model
 {
+    protected $fillable = ['parent_id' , 'child_id'];
+
     protected $hidden = [
         'created_at', 'updated_at'
     ];
