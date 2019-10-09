95fa1a8f2497b9f57389caace76c1884f7eb021e
Hoda1321 <Hoda1321@bitbucket.orggit config --global user.email Hoda1321@bitbucket.orggit config --global user.email Hoda1321@bitbucket.org>
2019-10-09 11:22:19 +0200

    get announce by id

--- routes/api.php
+++ routes/api.php
@@ -51,9 +51,10 @@ Route::group(['middleware' => 'auth:api'], function () {
         Route::post('delete', 'AnnouncementController@delete_announcement')->name('deleteannounce')->middleware('permission:announcements/delete');
         Route::post('announce', 'AnnouncementController@announcement')->name('announce')->middleware('permission:announcements/send');
         Route::get('get', 'AnnouncementController@get')->name('get')->middleware('permission:announcements/get');
-        Route::get('getByID', 'AnnouncementController@getAnnounceByID')->name('getbyid')->middleware('permission:announcements/getbyid');
         Route::post('update', 'AnnouncementController@update_announce')->name('updateannounce')->middleware('permission:announcements/update');
+        Route::get('getByID', 'AnnouncementController@getAnnounceByID')->name('getbyid')->middleware('permission:announcements/getbyid');
     });
+    Route::get('getByID', 'AnnouncementController@getAnnounceByID');
 
     //Calendar Route
     Route::post('calendar','CalendarController@Calendar')->name('calendar')->middleware('permission:calendar/get');
