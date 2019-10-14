143b59a7b08bfa13490a26bfa40ade5ce16a376a
Hoda1321 <Hoda1321@bitbucket.orggit config --global user.email Hoda1321@bitbucket.orggit config --global user.email Hoda1321@bitbucket.org>
2019-10-14 12:36:50 +0200

    add grade category and max attemp

--- Modules/QuestionBank/Routes/api.php
+++ Modules/QuestionBank/Routes/api.php
@@ -24,7 +24,7 @@ Route::group(['prefix' => 'quiz', 'middleware' => 'auth:api'], function () {
 
     //Quiz Routes
     Route::post('deleteQuiz', 'QuizController@destroy')->middleware('permission:quiz/delete');
-    Route::get('getQuiz', 'QuizController@get')->name('getquiz');//->middleware('permission:quiz/get');
+    Route::get('getQuiz', 'QuizController@get')->name('getquiz')->middleware('permission:quiz/get');
     Route::get('sort', 'QuizController@sort')->middleware('permission:quiz/sort');
     Route::get('SortUp', 'QuizController@SortUp')->middleware('permission:quiz/sortup');
 
