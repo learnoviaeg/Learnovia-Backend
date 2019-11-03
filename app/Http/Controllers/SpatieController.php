26408268b64af81f69e6a534fe1723fc6189a9b2
bugzeenut <hossam.ahmed@learnovia.com>
2019-11-03 15:20:45 +0200

    adding description to the role and adding count

--- app/Http/Controllers/SpatieController.php
+++ app/Http/Controllers/SpatieController.php
@@ -164,6 +164,12 @@ class SpatieController extends Controller
             //Import
             \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'import', 'title' => 'import excel sheet']);
 
+            //Language Permission
+            \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'languages/activate', 'title' => 'Activate Language']);
+            \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'languages/deactivate', 'title' => 'Deactivate Language']);
+            \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'languages/add', 'title' => 'Add Language']);
+            \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'languages/set-default', 'title' => 'Set default Language']);
+
             //Lesson Permissions
             \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'lesson/add', 'title' => 'add lesson']);
             \Spatie\Permission\Models\Permission::create(['guard_name' => 'api', 'name' => 'lesson/get', 'title' => 'get lesson']);
