4b5fddd34e854bc0b5fd6c5b13bfb728a2afaca4
salma01 <salma.amr@learnovia.com>
2019-11-06 15:59:49 +0200

    Get depth and path

--- app/GradeCategory.php
+++ app/GradeCategory.php
@@ -24,4 +24,52 @@ class GradeCategory extends Model
         return $this->hasMany('App\GradeItems','grade_category','id');
     }
 
+    public static function Depth($id){
+        $ParentOfGivenGradeCat=self::where('id',$id)->first('parent');
+            $i=1;
+            $Flag=true;
+            if(is_null($ParentOfGivenGradeCat->parent)){
+                return $i;
+            }else{
+            $i+=1;
+                $CurrentParent=self::where('id',$ParentOfGivenGradeCat->parent)->first('parent');
+                if($CurrentParent->parent!=null){
+                    $i++;
+                    while($Flag==true){
+                        $CurrentParent=self::where('id',$CurrentParent->parent)->first('parent');
+                        if($CurrentParent->parent==null){
+                            $Flag=False;
+                            return $i;
+                        }else
+                        $i++;                    
+                    }
+                }            
+            return $i;
+            }
+    }
+
+    public static function Path($id){
+        $ParentOfGivenGradeCat=self::where('id',$id)->first();
+            $Flag=true;
+            $path=array();
+            array_push($path,$ParentOfGivenGradeCat);
+            if(is_null($ParentOfGivenGradeCat->parent)){
+                return $path;
+            }else{
+                $CurrentParent=self::where('id',$ParentOfGivenGradeCat->parent)->first();
+                array_push($path,$CurrentParent);
+                if($CurrentParent->parent!=null){
+                    array_push($path,$CurrentParent);
+                    while($Flag==true){
+                        $CurrentParent=self::where('id',$CurrentParent->parent)->first();
+                        array_push($path,$CurrentParent);
+                        if($CurrentParent->parent==null){
+                            $Flag=False;
+                        }                    
+                    }
+                }            
+            return $path;
+            }
+    }
 }
+   
