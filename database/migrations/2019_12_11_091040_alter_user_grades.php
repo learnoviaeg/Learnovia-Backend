46136f415ed46e780e38f117c266e57536e59c43
bugzeenut <hossam.ahmed@learnovia.com>
2019-12-11 15:39:49 +0200

    change class name

--- database/migrations/2019_12_11_091040_alter_user_grades.php
+++ database/migrations/2019_12_11_091040_alter_user_grades.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterUserGrades extends Migration
+class AlterUserGrades2 extends Migration
 {
     /**
      * Run the migrations.
