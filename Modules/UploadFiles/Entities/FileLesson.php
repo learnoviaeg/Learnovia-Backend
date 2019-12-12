4e217d2f681b4e24082514724304bbbaa73ac3b3
bugzeenut <hossam.ahmed@learnovia.com>
2019-12-12 13:15:49 +0200

    some fixes of file return objects

--- Modules/UploadFiles/Entities/FileLesson.php
+++ Modules/UploadFiles/Entities/FileLesson.php
@@ -15,4 +15,10 @@ class FileLesson extends Model
     {
         return $this->hasMany('Modules\UploadFiles\Entities\File', 'id', 'file_id');
     }
+
+    public static function getNextIndex($lesson_id){
+        if(self::whereLesson_id($lesson_id)->max('index') == null)
+            return 1;
+        return self::whereLesson_id($lesson_id)->max('index') + 1;
+    }
 }
