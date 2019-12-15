be7bffd9eac535443b3c4b0c13801d4950757937
bugzeenut <hossam.ahmed@learnovia.com>
2019-12-15 12:12:41 +0200

    fix some fuckin routing issues in attendance module

--- Modules/Attendance/Routes/api.php
+++ Modules/Attendance/Routes/api.php
@@ -14,15 +14,15 @@ use Illuminate\Http\Request;
 */
 Route::group(['prefix' => 'attendance', 'middleware' => 'auth:api'], function () {
     Route::get('install','AttendanceController@install');
-    Route::post('add', 'AttendanceController@create')->name('addattendance')->middleware('permission:add');
-    Route::post('add-log', 'AttendanceLogController@create');//->name('addattendancelog')->middleware('permission:add-log');
-    Route::post('get-users-in-attendence', 'AttendanceController@get_all_users_in_attendence')->name('getusersinattendence')->middleware('permission:get-users-in-attendence');
-    Route::get('view-students-in-session', 'AttendanceController@viewstudentsinsessions');//->name('getusersinsession')->middleware('permission:view-students-in-session');
-    Route::post('get-users-taken-in-session', 'AttendanceController@get_all_taken_users_in_session')->name('getuserstakeninsession')->middleware('permission:get-users-taken-in-session');
-    Route::post('add-session', 'AttendanceController@createSession');//->name('getuserstakeninsession')->middleware('permission:get-users-taken-in-session');
-});
-Route::group(['prefix' => 'status', 'middleware' => 'auth:api'], function () {
-    Route::post('AddStatus','StatusController@Add');
-    Route::post('UpdateStatus','StatusController@Update');
-    Route::post('DeleteStatus','StatusController@Delete');
+    Route::post('add', 'AttendanceController@create')->name('addattendance')->middleware('permission:attendance/add');
+    Route::post('add-log', 'AttendanceLogController@create')->name('addattendancelog')->middleware('permission:attendance/add-log');
+    Route::post('get-users-in-attendence', 'AttendanceController@get_all_users_in_attendence')->name('getusersinattendence')->middleware('attendance/permission:get-users-in-attendence');
+    Route::get('view-students-in-session', 'AttendanceController@viewstudentsinsessions')->name('getusersinsession')->middleware('permission:attendance/view-students-in-session');
+    Route::post('get-users-taken-in-session', 'AttendanceController@get_all_taken_users_in_session')->name('getuserstakeninsession')->middleware('permission:attendance/get-users-taken-in-session');
+    Route::post('add-session', 'AttendanceController@createSession')->name('getuserstakeninsession')->middleware('permission:attendance/get-users-taken-in-session');
+    Route::group(['prefix' => 'status', 'middleware' => 'auth:api'], function () {
+        Route::post('add','StatusController@Add')->middleware('attendance/status/add');
+        Route::post('update','StatusController@Update')->middleware('attendance/status/update');
+        Route::post('delete','StatusController@Delete')->middleware('attendance/status/delete');
+    });
 });
