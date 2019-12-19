9a2f9c374dd15c9723f277f9aab8c7403681c7cd
Hend_Genady <hend.genady@learnovia.com>
2019-12-19 11:07:43 +0200

    update user when enroll mandatory

--- app/Observers/EnrollObserver.php
+++ app/Observers/EnrollObserver.php
@@ -10,11 +10,10 @@ class EnrollObserver
 {
     public function created(Enroll $enroll)
     {
-        // dd($enroll);
         $user = User::find($enroll->user_id);
-        // dd($user);
+        
         $courseSeg=CourseSegment::find($enroll->course_segment);
-        // dd($courseSeg->segmentClasses);
+
         $user->update([
             'class_id' => $courseSeg->segmentClasses[0]->classLevel[0]->class_id,
             'level' => $courseSeg->segmentClasses[0]->classLevel[0]->yearLevels[0]->level_id,
