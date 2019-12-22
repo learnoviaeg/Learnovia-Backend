8d2e620e1f33ea740d6d69b9e167c676d222e9df
bugzeenut <hossam.ahmed@learnovia.com>
2019-12-22 12:12:54 +0200

    allow null to GC in quiz lesson

---
+++ Modules/QuestionBank/Database/Migrations/2019_12_22_121029_allow_null_grade_category_to_quiz_lesson_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AllowNullGradeCategoryToQuizLessonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quiz_lessons', function (Blueprint $table) {
+            $table->dropColumn('grade_category_id');
+        });
+
+        Schema::table('quiz_lessons', function (Blueprint $table) {
+            $table->unsignedBigInteger('grade_category_id')->nullable(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quiz_lesson', function (Blueprint $table) {
+            //
+        });
+    }
+}
