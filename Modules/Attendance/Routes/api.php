8722c72376349569415b23caec515411cdef405f
bugzeenut <hossam.ahmed@learnovia.com>
2020-01-09 14:12:54 +0200

    fix attendance conflict

--- Modules/Attendance/Routes/api.php
+++ Modules/Attendance/Routes/api.php
@@ -18,8 +18,8 @@ Route::group(['prefix' => 'attendance', 'middleware' => 'auth:api'], function ()
     Route::post('add-log', 'AttendanceLogController@create')->name('addattendancelog')->middleware('permission:attendance/add-log');
     Route::post('get-users-in-attendence', 'AttendanceController@get_all_users_in_attendence')->name('getusersinattendence')->middleware('permission:attendance/get-users-in-attendence');
     Route::get('get-attendance', 'AttendanceController@getAttendance')->name('getattendence')->middleware('permission:attendance/get-attendence');
-    Route::get('delete-attendance', 'AttendanceController@deleteAttendance')->name('deleteattendence')->middleware('permission:attendance/delete-attendance');
-    Route::get('edit-attendance', 'AttendanceController@editAttendance')->name('editattendence')->middleware('permission:attendance/edit-attendence');
+    Route::post('delete-attendance', 'AttendanceController@deleteAttendance')->name('deleteattendence')->middleware('permission:attendance/delete-attendance');
+    Route::post('edit-attendance', 'AttendanceController@editAttendance')->name('editattendence')->middleware('permission:attendance/edit-attendence');
     Route::get('view-students-in-session', 'AttendanceController@viewstudentsinsessions')->name('getusersinsession')->middleware('permission:attendance/get-users-in-session');
     Route::post('get-users-taken-in-session', 'AttendanceController@get_all_taken_users_in_session')->name('getuserstakeninsession')->middleware('permission:attendance/get-users-taken-in-session');
     Route::post('add-session', 'AttendanceController@createSession')->name('addsession')->middleware('permission:attendance/add-session');
@@ -28,9 +28,6 @@ Route::group(['prefix' => 'attendance', 'middleware' => 'auth:api'], function ()
     Route::post('update-session', 'AttendanceController@update_session')->name('updatesession')->middleware('permission:attendance/update-session');
     Route::post('delete-session', 'AttendanceController@delete_session')->name('deletesession')->middleware('permission:attendance/delete-session');
     Route::get('get-session', 'AttendanceController@get_session_byID')->name('getsessionbyid')->middleware('permission:attendance/get-session-by-id');
-
-    
-    
     Route::group(['prefix' => 'status', 'middleware' => 'auth:api'], function () {
         Route::get('get','StatusController@get')->middleware('permission:attendance/status/get');
         Route::post('add','StatusController@Add')->middleware('permission:attendance/status/add');
