41518d3fafef565d903b9998a8dac0854e838ea6
bugzeenut <hossam.ahmed@learnovia.com>
2020-01-28 11:03:40 +0200

    hot change password for all teachers

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,5 @@ use App\User;
 use Spatie\Permission\Models\Role;
 
 Route::get('test' , function(){
-    User::where('id' , '>' , '889')->get()->each(function($user){
-        $user->updated([
-            'password' => bcrypt(123456),
-            'real_password' => 123456
-        ]
-        );
-    });
+    return User::where('id' , '>' , 889)->get();
 });
