277d323847548dd7a6acab66b25b126fdd8d9894
salma <salma.amr@learnovia.com>
2020-02-03 13:22:45 +0000

    QuizLessonController.php edited online with Bitbucket

--- Modules/QuestionBank/Http/Controllers/QuizLessonController.php
+++ Modules/QuestionBank/Http/Controllers/QuizLessonController.php
@@ -37,10 +37,10 @@ class QuizLessonController extends Controller
             'grading_method_id' => 'required',
             'grade' => 'required|integer|min:1',
             'graded' => 'required|boolean',
-            // 'grade_category_id' => 'integer|exists:grade_categories,id',
-            // 'grade_min' => 'integer',
-            // 'grade_max' => 'integer',
-            // 'grade_to_pass' => 'integer',
+            'grade_category_id' => 'integer|exists:grade_categories,id',
+             //'grade_min' => 'integer',
+            //'grade_max' => 'integer',
+            //'grade_to_pass' => 'integer',
         ]);
 
         $quiz = quiz::find($request->quiz_id);
@@ -52,11 +52,11 @@ class QuizLessonController extends Controller
             $lesson = Lesson::find($lessons);
             $gradeCats= $lesson->courseSegment->GradeCategory;
             $flag= false;
-            // foreach ($gradeCats as $grade){
-            //     if($grade->id==$request->grade_category_id){
-            //         $flag =true;
-            //     }
-            // }
+             foreach ($gradeCats as $grade){
+                 if($grade->id==$request->grade_category_id){
+                    $flag =true;
+                 }
+             }
             $course_Quiz=CourseSegment::where('id',$quiz->course_segment_id)->pluck('course_id')->first();
             $coueseSegment = $lesson->courseSegment;
             if($course_Quiz != $coueseSegment->course_id){
@@ -78,7 +78,7 @@ class QuizLessonController extends Controller
                 'max_attemp' => $request->max_attemp,
                 'grading_method_id' => $request->grading_method_id,
                 'grade' => $request->grade,
-                // 'grade_category_id' => $request->grade_category_id,
+                'grade_category_id' => $request->grade_category_id,
                 'publish_date' => $request->opening_time
             ]);
 
@@ -94,25 +94,25 @@ class QuizLessonController extends Controller
             ]);
             // return $requ;
             $ss= user::notify($requ);
-            // if($request->graded == true){
-            //     if($flag==false){
-            //         return HelperController::api_response_format(400, null,'this grade category invalid');
-            //     }
-            //     $grade_category=GradeCategory::find($request->grade_category_id);
-            //     $grade_category->GradeItems()->create([
-            //         'grademin' => (isset($request->grade_min)) ? $request->grade_min : null,
-            //         'grademax' => (isset($request->grade_max)) ? $request->grade_max : null,
-            //         'item_no' => 1,
-            //         'scale_id' => (isset($request->scale_id)) ? $request->scale_id : null,
-            //         'grade_pass' => (isset($request->grade_pass)) ? $request->grade_pass : null,
-            //         'aggregationcoef' => (isset($request->aggregationcoef)) ? $request->aggregationcoef : null,
-            //         'aggregationcoef2' => (isset($request->aggregationcoef2)) ? $request->aggregationcoef2 : null,
-            //         'item_type' => (isset($request->item_type)) ? $request->item_type : null,
-            //         'item_Entity' => $quizLesson[0]->id,
-            //         'name' => $quizLesson[0]->quiz->name,
-            //         'weight' => 0,
-            //     ]);
-            // }
+             if($request->graded == true){
+                 if($flag==false){
+                    return HelperController::api_response_format(400, null,'this grade category invalid');
+                 }
+                 $grade_category=GradeCategory::find($request->grade_category_id);
+                 $grade_category->GradeItems()->create([
+                     'grademin' => (isset($request->grade_min)) ? $request->grade_min : null,
+                     'grademax' => (isset($request->grade_max)) ? $request->grade_max : null,
+                     'item_no' => 1,
+                     'scale_id' => (isset($request->scale_id)) ? $request->scale_id : null,
+                     'grade_pass' => (isset($request->grade_pass)) ? $request->grade_pass : null,
+                    'aggregationcoef' => (isset($request->aggregationcoef)) ? $request->aggregationcoef : null,
+                     'aggregationcoef2' => (isset($request->aggregationcoef2)) ? $request->aggregationcoef2 : null,
+                     'item_type' => (isset($request->item_type)) ? $request->item_type : null,
+                     'item_Entity' => $quizLesson[0]->id,
+                     'name' => $quizLesson[0]->quiz->name,
+                     'weight' => 0,
+                 ]);
+             }
             LessonComponent::create([
                 'lesson_id' => $lessons,
                 'comp_id'   => $request->quiz_id,
