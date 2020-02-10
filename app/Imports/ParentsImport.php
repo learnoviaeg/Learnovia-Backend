9de724468cc816007d28fd22c8a566aefae47f1c
Hend_Genady <hend.genady@learnovia.com>
2020-02-10 09:42:39 +0200

    Parents import

--- app/Imports/ParentsImport.php
+++ app/Imports/ParentsImport.php
@@ -23,33 +23,22 @@ class ParentsImport implements ToModel, WithHeadingRow
 
         $password = mt_rand(100000, 999999);
 
-        // $user =new User();
-        // $user->firstname = $row['firstname'];
-        // $user->lastname = $row['lastname'];
-        // $user->username = User::generateUsername();
-        // $user->password = bcrypt($password);
-        // $user->real_password = $password;
-        // $user->save();
-
-        $user =new User([
+        $user =User::firstOrCreate([
             'firstname' => $row['firstname'],
             'lastname' => $row['lastname'],
             'username' => User::generateUsername(),
             'password' => bcrypt($password),
             'real_password' => $password
         ]);
-        $user->save();
 
-        // dd($user);
         $childs=User::where('lastname',$row['fullname'])->pluck('id');
 
         foreach($childs as $child)
         {
-            $parent = new Parents([
+            $parent = Parents::firstOrCreate([
                 'parent_id' => $user->id,
                 'child_id' => $child,
             ]);
-            $parent->save();
         }
     }
 }
