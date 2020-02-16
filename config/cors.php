8c90cadfc813b1bd7435d941f63317a68ca33af8
Hossam <hossam.ahmed@learnovia.com>
2020-02-16 08:22:14 +0000

    cors.php edited online with Bitbucket

--- config/cors.php
+++ config/cors.php
@@ -17,10 +17,10 @@ return [
      */
     'default_profile' => [
 
-        //'allow_credentials' => false,
+        'allow_credentials' => true,
 
         'allow_origins' => [
-           '*'
+           'http://localhost:80'
         ],
 
         'allow_methods' => [
