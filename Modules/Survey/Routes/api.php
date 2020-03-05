ae7e8824b7643e5266e3af83e859633dc8de021b
salma01 <salma.amr@learnovia.com>
2020-03-05 12:22:59 +0200

    sur

--- Modules/Survey/Routes/api.php
+++ Modules/Survey/Routes/api.php
@@ -18,7 +18,7 @@ Route::group(['prefix' => 'survey', 'middleware' => 'auth:api'], function () {
     Route::get('install', 'SurveyController@install_survey');
     Route::post('add', 'SurveyController@store')->middleware('permission:survey/add');
     Route::post('submit', 'UserSurveyController@submitSurvey')->middleware('permission:survey/add-question');
-    Route::get('my-surveys', 'UserSurveyController@get_my_surveys')->middleware('permission:survey/my-surveys');
+    Route::get('my-surveys', 'UserSurveyController@get_my_surveys');//->middleware('permission:survey/my-surveys');
     Route::get('view-all-submissions', 'UserSurveyController@Review_all_Submissions_of_survey')->middleware('permission:survey/view-all-submissions');
     Route::get('get-template', 'SurveyController@get_template')->middleware('permission:template/get');
 });
