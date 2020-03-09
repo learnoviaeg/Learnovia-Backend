92f1f73fb4c85e1f3d8442097f940b8148d31854
Hend_Genady <hend.genady@learnovia.com>
2020-03-09 12:19:23 +0200

    validate username

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Huda
@@ -56,7 +57,7 @@ class UserController extends Controller
             'password.*' => 'required|string|min:6|max:191',
             // 'role' => 'required|array',
             // 'role.*' => 'required|exists:roles,id',
-            'role' => 'requird|exists:roles,id', /// in all system
+            'role' => 'required|exists:roles,id', /// in all system
             'role_id' => 'required_with:level|exists:roles,id', /// chain role
             'optional.*' => 'exists:courses,id',
             'optional' => 'array',
@@ -177,6 +178,7 @@ class UserController extends Controller
             'id' => 'required|exists:users,id',
             'email' => 'unique:users,email,'.$request->id,
             'password' => 'string|min:6|max:191',
+            'username' => 'unique:users,username',
             'role' => 'exists:roles,id', /// in all system
             'role_id' => 'required_with:level|exists:roles,id', /// chain role
         ]);
@@ -193,18 +195,18 @@ class UserController extends Controller
         $check = $user->update([
             'firstname' => $request->firstname,
             'lastname' => $request->lastname,
-            'username' => User::generateUsername(),
         ]);
 
-        if (isset($request->password))
             if (Auth::user()->can('user/update-password')) {
-                $user->real_password=$request->password;
-                $user->password =   bcrypt($request->password);
+                if (isset($request->password)){
+                    $user->real_password=$request->password;
+                    $user->password =   bcrypt($request->password);
+                }
             }
 
-        if (isset($request->username))
             if (Auth::user()->can('user/update-username')) {
-                $user->username=$request->username;
+                if (isset($request->username))
+                    $user->username=$request->username;
             }
 
         if (isset($request->picture))
