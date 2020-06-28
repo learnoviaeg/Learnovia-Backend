f50f4e5c88e2faab7161e16e21726ca563c88a39
salma01 <salma.amr@learnovia.com>
2020-06-28 11:33:47 +0200

    lang table

---
+++ app/Language.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+    protected $fillable = ['name','default'];
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+}
