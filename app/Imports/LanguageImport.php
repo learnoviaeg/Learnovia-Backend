409424c5e34df64ba5f2daee2121c21844bfcd93
Hend_Genady <hend.genady@learnovia.com>
2020-07-08 11:48:21 +0200

    translate to arabic

---
+++ app/Imports/LanguageImport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Imports;
+
+use App\Dictionary;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Validator;
+
+class LanguageImport implements ToModel , WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        Validator::make($row,[
+            'language' => 'exists:languages,id'
+        ])->validate();
+
+        return  new Dictionary([
+            'language' => $row['language'],
+            'key' => $row['key'],
+            'value' => $row['value'],
+        ]);
+    }
+}
