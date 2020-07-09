6fa6a6c7b5a761b39c1bb79e6b78ca656fd0a528
Hend_Genady <hend.genady@learnovia.com>
2020-07-09 09:57:04 +0200

    password just numbers

--- app/User.php
+++ app/User.php
@@ -84,15 +84,8 @@ class User extends Authenticatable
     }
     public static function generatePassword()
     {
-
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $pass = array(); //remember to declare $pass as an array
-        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-        for ($i = 0; $i < 8; $i++) {
-            $n = rand(0, $alphaLength);
-            $pass[] = $alphabet[$n];
-        }
-        return implode($pass);
+        $pass = rand(0, 99999999);
+        return $pass;
     }
 
     public function roles()
