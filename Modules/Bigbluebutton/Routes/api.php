a7ad5e23341794e9255d13023a4e72510afd9878
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2020-08-17 15:31:31 +0200

    get bbb attendnace

--- Modules/Bigbluebutton/Routes/api.php
+++ Modules/Bigbluebutton/Routes/api.php
@@ -27,6 +27,7 @@ Route::group(['prefix' => 'bigbluebutton', 'middleware' => 'auth:api'], function
     Route::get('delete', 'BigbluebuttonController@destroy')->name('delete')->middleware('permission:bigbluebutton/delete');
     Route::get('toggle', 'BigbluebuttonController@toggle')->name('toggle')->middleware('permission:bigbluebutton/toggle');
     Route::get('attendance', 'BigbluebuttonController@takeattendance')->name('takeattendance')->middleware('permission:bigbluebutton/attendance');
+    Route::get('get-attendance', 'BigbluebuttonController@viewAttendence')->name('get-attendance')->middleware('permission:bigbluebutton/get-attendance');
     Route::get('meetinginfo', 'BigbluebuttonController@getmeetingInfo')->name('getmeetingInfo');
 
 
