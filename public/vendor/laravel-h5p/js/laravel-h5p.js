b0809bbf7316738eb8ca780939fe329cc954a0b2
Hoda1321 <huda.mahmoud@learnovia.com>
2020-08-17 16:22:19 +0200

    remove post message and updated

--- public/vendor/laravel-h5p/js/laravel-h5p.js
+++ public/vendor/laravel-h5p/js/laravel-h5p.js
@@ -59,8 +59,8 @@
             }
 
             $('#laravel-h5p-form').submit(function () {
-                alert('hii from backend one');
-                document.getElementById('myIframe').contentWindow.postMessage("hii","https://dev.learnovia.com/");
+                // alert('hii from backend one');
+                // document.getElementById('myIframe').contentWindow.postMessage("hii","https://dev.learnovia.com/");
                 if (h5peditor !== undefined) {
                     var params = h5peditor.getParams();
                     if (params !== undefined) {
