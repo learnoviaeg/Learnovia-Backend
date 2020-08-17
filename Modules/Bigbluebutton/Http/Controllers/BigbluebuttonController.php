725a429583fca01ad118c9dc9a543bd055e1c65f
mirna mourad <mirna.mourad@learnovia.com>
2020-08-17 10:26:54 +0000

    bbb test

--- Modules/Bigbluebutton/Http/Controllers/BigbluebuttonController.php
+++ Modules/Bigbluebutton/Http/Controllers/BigbluebuttonController.php
@@ -464,14 +464,12 @@ class BigbluebuttonController extends Controller
                 $atendees=collect();
                 $names=collect();
                 if(count($response['attendees']) >1){
-                  foreach($response['attendees']['attendee'] as $attend){
-                    
-                        $names->push($attend['fullName']);
-                    }
-                    // foreach($atendees[0] as $mm){
-                        
-                    //     $names->push($mm['fullName']);
-                    // }
+                   foreach($response['attendees'] as $attend){
+                    $atendees->push($attend);
+                   }
+                  foreach($atendees[0] as $mm){
+                    $names->push($mm['fullName']);
+                   }
                 }else{
                     foreach($response['attendees'] as $attend){
                     
