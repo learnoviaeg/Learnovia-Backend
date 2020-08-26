61e7d927bb7afb73fbc2f4a0cd5ad217eee79f49
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2020-08-26 09:19:04 +0200

    clear

--- Modules/Bigbluebutton/Http/Controllers/BigbluebuttonController.php
+++ Modules/Bigbluebutton/Http/Controllers/BigbluebuttonController.php
@@ -680,7 +680,6 @@ class BigbluebuttonController extends Controller
     public function clear(){
         // \Artisan::call('config:cache');
         \Artisan::call('cache:clear', ['--env' => 'local']);
-        
         \Artisan::call('config:clear', ['--env' => 'local']);
     }
 }
