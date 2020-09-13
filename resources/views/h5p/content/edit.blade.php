4818b37fadf19ed6d3c0030b0587b77ef38a933e
Hoda1321 <huda.mahmoud@learnovia.com>
2020-09-13 09:50:48 +0200

    add editting done

--- resources/views/h5p/content/edit.blade.php
+++ resources/views/h5p/content/edit.blade.php
@@ -9,10 +9,10 @@
     <div class="container-fluid">
         <script>
             function postData(){
-            let msg = JSON.stringify( {'data': {{ $id }}, 'type': 'edit' });
+            let msg = JSON.stringify( {'data': {{ $id }}, 'type': 'edit' ,'editting_done':{{ $editting_done }} });
                 window.parent.postMessage( msg ,'*');
             }
-            let msg2 = JSON.stringify( {'data': {{ $id }}, 'type': 'create' });
+            let msg2 = JSON.stringify( {'data': {{ $id }}, 'type': 'create' ,'editting_done':{{  $editting_done}} });
             window.parent.postMessage(msg2 ,'*');            
             </script>
     
@@ -20,7 +20,7 @@
     
             <div class="col-md-12">
                
-                {!! Form::model($content, ['method' => 'PATCH','route' => ['h5p.update', $id], 'class'=>'form-horizontal', 'id'=>'laravel-h5p-form', 'enctype'=>"multipart/form-data"]) !!}
+                {!! Form::model($content, ['method' => 'PATCH','route' => ['h5p.update', $id,$editting_done], 'class'=>'form-horizontal', 'id'=>'laravel-h5p-form', 'enctype'=>"multipart/form-data"]) !!}
                 <input type="hidden" name="library" id="laravel-h5p-library" value="{{ $library }}">
                 <input type="hidden" name="parameters" id="laravel-h5p-parameters" value="{{ $parameters }}">
                 <fieldset>
