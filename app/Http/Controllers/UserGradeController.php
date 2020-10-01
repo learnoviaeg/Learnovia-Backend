91bf7859a612d7ea50448018cf9e40d012c3ef52
Hend_Genady <hend.genady@learnovia.com>
2020-10-01 15:27:44 +0200

    not update grade if there is an essay qustion not correct

--- app/Http/Controllers/UserGradeController.php
+++ app/Http/Controllers/UserGradeController.php
@@ -153,7 +153,7 @@ class UserGradeController extends Controller
         $grades = [];
         $ids = [];
         foreach ($users as $user) {
-            UserGrade::quizUserGrade($user);
+            return UserGrade::quizUserGrade($user);
             $user->grades = collect();
             $i = 0;
             if(isset($user->attachment))
