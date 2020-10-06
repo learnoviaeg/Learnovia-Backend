6b345f82f16021573e2188dc0addc4be2147e85b
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2020-10-06 12:53:34 +0200

    chat room

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -12,25 +12,14 @@ class ChatController extends Controller
     {
         $request->validate([
             'participants' => 'array|required',
-            'participants.*' => 'string',//|exists:users,chat_uid',
+            'participants.*' => 'string|exists:users,chat_uid',
             'name'=>'string',
             'text'=>'string',
-
         ]);
 
-        $client = new \Google_Client();
-        $client->setAuthConfig(base_path('learnovia-notifications-firebase-adminsdk-z4h24-17761b3fe7.json'));
-        $client->setApplicationName("learnovia-notifications");
-        $client->setScopes(['https://www.googleapis.com/auth/firebase.messaging']);
-        $client->useApplicationDefaultCredentials();
-        if ($client->isAccessTokenExpired()) {
-            $client->fetchAccessTokenWithAssertion();
-        }
-        $access_token = $client->getAccessToken()['access_token']; 
         $clientt = new Client();
         $data = array(
-            'participants' => $request->participants,
-        
+            'participants' => $request->participants
         );
         if(isset($request->name)){
             $data['name'] = $request->name;
@@ -45,11 +34,11 @@ class ChatController extends Controller
         $data = json_encode($data);
         $res = $clientt->request('POST', 'https://us-central1-akwadchattest.cloudfunctions.net/createRoom', [
             'headers'   => [
-                'Authorization' => 'Bearer '. $access_token,
                 'Content-Type' => 'application/json'
             ], 
             'body' => $data
         ]);
-        return $res; 
+        
+        return json_decode($res->getBody(),true);
     }
 }
