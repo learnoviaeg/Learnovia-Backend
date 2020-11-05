e5dcd80be18deff5588e56f857b59d165e483b3c
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2020-11-05 09:10:02 +0200

    resoource test

--- app/Level.php
+++ app/Level.php
@@ -40,7 +40,7 @@ class Level extends Model
         return $ids;
     }
     protected $hidden = [
-        'created_at','updated_at'
+        'created_at','updated_at','deleted_at'
     ];
 
     public function timeline()
