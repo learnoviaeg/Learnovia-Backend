ad7fbc147c511331a92d887c274a45c1eedc9b89
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2020-11-08 10:02:40 +0200

    course resource

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -100,18 +100,7 @@ class CoursesController extends Controller
      */
     public function show($id)
     {
-        $course = Course::whereId($id);
-
-        $teachers = $course->with(['courseSegments.enroll.user'])->get()
-                           ->pluck('courseSegments.*.enroll.*')->collapse()
-                           ->where('role_id',4)->pluck('user')->unique()->values();
-
-        $classes = $course->with(['courseSegments.enroll.classes'])->get()
-                          ->pluck('courseSegments.*.enroll.*')->collapse()->pluck('classes')->unique()->values();
-        return $course->with(['courseSegments.enroll.classes'])->get();
-        // return $course;with(['courseSegments.enroll.user'])
-
-
+        //
     }
 
     /**
