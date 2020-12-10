63105a4a6b240aa43921948b24963521a9cb6537
Hend_Genady <hend.genady@learnovia.com>
2020-12-10 15:31:51 +0200

    enroll resourc

--- routes/api.php
+++ routes/api.php
@@ -444,6 +444,8 @@ Route::group(['middleware' => ['auth:api','LastAction']], function () {
     Route::Resource('notify', NotificationsController::class);
     Route::Resource('announcement', AnnouncementsController::class);
     Route::get('announcements/{created}', 'AnnouncementsController@index')->middleware('permission:announcements/my');
+    Route::delete('enroll/', 'EnrollController@destroy');
+    Route::Resource('enroll', EnrollController::class);
 });
 
 Route::group(['middleware' => ['auth:api']], function () {
