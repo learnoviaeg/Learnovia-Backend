5ef351228635087338a93c4749e3310c63389569
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2020-12-16 15:17:10 +0200

    done

--- app/Listeners/MassLogsListener.php
+++ app/Listeners/MassLogsListener.php
@@ -9,22 +9,19 @@ use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\Log;
 use App\User;
-use App\Announcement;
-use App\AnnouncementsChain;
-use App\userAnnouncement;
-use DB;
-use App\Enroll;
+use App\Repositories\EnrollmentRepositoryInterface;
 
 class MassLogsListener
 {
+    protected $unEnroll;
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(EnrollmentRepositoryInterface $unEnroll)
     {
-        //
+        $this->unEnroll = $unEnroll;
     }
 
     /**
@@ -59,54 +56,7 @@ class MassLogsListener
                 ]);
 
                 if($log->model == 'Enroll'){
-
-                    $user_enrolls = Enroll::where('user_id',$one->user_id)->where('id','!=',$one->id)->count();
-                    if($user_enrolls == 0){
-                        userAnnouncement::where('user_id',$one->user_id)->delete();
-                        DB::table('notifications')->where('notifiable_id', $one->user_id)->delete();    
-                    }
-
-                    if($user_enrolls != 0){
-
-                        $enrolls =  Enroll::where('user_id',$one->user_id)->where('id','!=',$one->id)->get();
-
-                        $user_old_announcements = Announcement::whereNotIn('year_id',$enrolls->pluck('year')->filter())
-                                                                ->orWhere('type_id',$enrolls->pluck('type')->filter())
-                                                                ->orWhere('level_id',$enrolls->pluck('level')->filter())
-                                                                ->orWhere('class_id',$enrolls->pluck('class')->filter())
-                                                                ->orWhere('segment_id',$enrolls->pluck('segment')->filter())
-                                                                ->orWhere('course_id',$enrolls->pluck('course')->filter())
-                                                                ->pluck('id');
-
-                        $user_old_announcements1 = AnnouncementsChain::whereNotIn('year',$enrolls->pluck('year')->filter())
-                                                                    ->orWhere('type',$enrolls->pluck('type')->filter())
-                                                                    ->orWhere('level',$enrolls->pluck('level')->filter())
-                                                                    ->orWhere('class',$enrolls->pluck('class')->filter())
-                                                                    ->orWhere('segment',$enrolls->pluck('segment')->filter())
-                                                                    ->orWhere('course',$enrolls->pluck('course')->filter())
-                                                                    ->pluck('id');
-
-                        $final_old_announcements = array_merge($user_old_announcements->toArray(),$user_old_announcements1->toArray());
-                        
-                        userAnnouncement::where('user_id',$one->user_id)->whereIn('announcement_id',$final_old_announcements)->delete();
-
-                        $notify = DB::table('notifications')->where('notifiable_id', $one->user_id)->get();
-                        $ids=collect();
-                        foreach ($notify as $not) {
-                            $not->data= json_decode($not->data, true);
-
-                            if($not->data['type'] == 'announcement' && in_array($not->data['id'],$final_old_announcements)){
-                                $ids->push($not->id);
-                            }
-
-                            if($not->data['type'] != 'announcement' && $not->data['course_id'] == $one->course){
-                                $ids->push($not->id);
-                            }
-                        }
-
-                        DB::table('notifications')->whereIn('id', $ids)->delete();
-                    }
-
+                    $this->unEnroll->RemoveAllDataRelatedToRemovedChain($one);
                 }
             }
                 
--- app/Observers/EnrollObserver.php
+++ app/Observers/EnrollObserver.php
@@ -10,12 +10,22 @@ use Carbon\Carbon;
 use App\Log;
 use App\User;
 use PhpParser\Node\Stmt\Continue_;
-use App\Announcement;
-use App\AnnouncementsChain;
-use App\userAnnouncement;
+use App\Repositories\EnrollmentRepositoryInterface;
 
 class EnrollObserver
 {
+    protected $unEnroll;
+
+    /**
+     * EnrollObserver constructor.
+     *
+     * @param EnrollmentRepositoryInterface $unEnroll
+     */
+    public function __construct(EnrollmentRepositoryInterface $unEnroll)
+    {
+        $this->unEnroll = $unEnroll;
+    }
+
     public function created(Enroll $enroll)
     {
         if ($enroll->courseSegment->courses[0]->mandatory == 1) {
@@ -68,45 +78,7 @@ class EnrollObserver
             'data' => serialize($req),
         ]);
 
-        $user_old_announcements = Announcement::where('year_id',$req->year)
-                                                ->where('type_id',$req->type)
-                                                ->where('level_id',$req->level)
-                                                ->where('class_id',$req->class)
-                                                ->where('segment_id',$req->segment)
-                                                ->where('course_id',$req->course)
-                                                ->pluck('id');
-
-        $user_old_announcements1 = AnnouncementsChain::where('year',$req->year)
-                                                ->where('type',$req->type)
-                                                ->where('level',$req->level)
-                                                ->where('class',$req->class)
-                                                ->where('segment',$req->segment)
-                                                ->where('course',$req->course)
-                                                ->pluck('announcement_id');
-
-        $final_old_announcements = array_merge($user_old_announcements->toArray(),$user_old_announcements1->toArray());
-
-        userAnnouncement::where('user_id',$req->user_id)->whereIn('announcement_id',$final_old_announcements)->delete();
-
-        $notify = DB::table('notifications')->where('notifiable_id', $req->user_id)->get();
-
-        foreach ($notify as $not) {
-            $not->data= json_decode($not->data, true);
-            if($not->data['type'] == 'announcement' && in_array($not->data['id'],$final_old_announcements->toArray())){
-                $not->delete();
-            }
-
-            if($not->data['type'] != 'announcement' && $not->data['course_id'] == $req->course){
-                $not->delete();
-            }
-        }
-
-        $user_enrolls = Enroll::where('user_id',$req->user_id)->where('id','!=',$req->id)->count();
-        if($user_enrolls == 0){
-            userAnnouncement::where('user_id',$req->user_id)->delete();
-            DB::table('notifications')->where('notifiable_id', $req->user_id)->delete();    
-        }
-
+        $this->unEnroll->RemoveAllDataRelatedToRemovedChain($req);
     }
 
     /**
