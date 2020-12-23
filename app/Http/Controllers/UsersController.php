7c51122a40167021060809c19a8c9d0fd1682d6a
Hoda1321 <huda.mahmoud@learnovia.com>
2020-12-23 10:39:33 +0200

    remove comment in permission

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,7 +23,7 @@ class UsersController extends Controller
     {
         $this->chain = $chain;
         $this->middleware('auth');
-        // $this->middleware(['permission:course/teachers|course/participants' , 'ParentCheck'],   ['only' => ['index']]);
+        $this->middleware(['permission:course/teachers|course/participants' , 'ParentCheck'],   ['only' => ['index']]);
     }
     /**
      * Display a listing of the resource.
