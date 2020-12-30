0f26ceacaab611fb4766431a2f4e4804429dcf55
Hend_Genady <hend.genady@learnovia.com>
2020-12-30 10:36:20 +0200

    logout when change username|password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -255,9 +255,9 @@ class UserController extends Controller
                 if (isset($request->password)){
                     $user->real_password=$request->password;
                     $user->password =   bcrypt($request->password);
+                    $user->token()->revoke();
                     $user->token=null;
                     $user->save();
-                    $user->token()->revoke();
                     Parents::where('parent_id',$user->id)->update(['current'=> 0]);
                 }
             }
@@ -265,9 +265,9 @@ class UserController extends Controller
             if (Auth::user()->can('user/update-username')) {
                 if (isset($request->username)){
                     $user->username=$request->username;
+                    $user->token()->revoke();
                     $user->token=null;
                     $user->save();
-                    $user->token()->revoke();
                     Parents::where('parent_id',$user->id)->update(['current'=> 0]);
                 }
             }
