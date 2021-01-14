68cde812afa9bf0dc2087fce9ba149bce3266c91
Hend_Genady <hend.genady@learnovia.com>
2021-01-14 10:33:06 +0200

    order layout by created_at

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -913,11 +913,11 @@ class CourseController extends Controller
                             if(isset($h5p_comp)){
                                 $h5p_content=collect();
                                 $url= substr($request->url(), 0, strpos($request->url(), "/api"));
-                                $h5p_all= $lessonn->H5PLesson->orderBy('created_at','desc');
+                                $h5p_all= $lessonn->H5PLesson;
                                 if ($request->user()->can('site/course/student')) {
                                     $h5p_all= $lessonn->H5PLesson->where('visible', '=', 1)->where('publish_date', '<=', Carbon::now());
                                 }
-                                foreach($h5p_all as $h5p){                                
+                                foreach($h5p_all->orderBy('created_at','desc') as $h5p){                                
                                     $content = response()->json(DB::table('h5p_contents')->whereId($h5p->content_id)->first());
                                     $content->original->link =  $url.'/api/h5p/'.$h5p->content_id;
                                     $content->original->item_lesson_id = $h5p->id;
