ecd5f28ad55aed787217dc86b8d12d44d5e38252
Hend_Genady <hend.genady@learnovia.com>
2021-04-28 00:27:55 +0200

    comprehension in 3 steps

--- Modules/QuestionBank/Entities/Quiz.php
+++ Modules/QuestionBank/Entities/Quiz.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class quiz extends Model
 {
-    protected $fillable = ['name','course_id','is_graded','duration','created_by' , 'shuffle','feedback'];
+    protected $fillable = ['name','course_id','is_graded','duration','created_by' , 'shuffle','feedback', 'draft'];
     protected $hidden = [
         'created_at', 'updated_at'
     ];
