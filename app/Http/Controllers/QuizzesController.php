6dd429a9a98b5d0f030d37b0d9c424853c0f15b6
Hend_Kagha <hend.genady@learnovia.com>
2021-06-09 13:15:03 +0200

    allow edit in quizzes

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -351,7 +351,7 @@ class QuizzesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id,Request $request)
     {
         $request->validate([
             'lesson_id' => 'required|exists:lessons,id',
