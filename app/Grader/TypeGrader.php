f588f0471aebb1e4ed9ec8a2fdbd938062271718
Hend_Kagha <hend.genady@learnovia.com>
2021-06-23 11:03:21 +0200

    mcq_correction

--- app/Grader/TypeGrader.php
+++ app/Grader/TypeGrader.php
@@ -7,12 +7,13 @@ class TypeGrader implements GraderInterface
 {
     public function True_False($answer)
     {
-        $grade['mark']=0;
-        $grade['right']=0;
+        $mark=0;
+        $right=0;
+        $grade=$answer['correct_answer'];
         if(isset($answer['student_answer']) && $answer['student_answer']->is_true == $answer['correct_answer']->is_true &&
             $answer['student_answer']->is_true != null){
-            $grade['mark']=$answer['correct_answer']->mark;
-            $grade['right']=1;
+            $grade->mark=$answer['correct_answer']->mark;
+            $grade->right=1;
         }
         return $grade;
     }
@@ -21,43 +22,66 @@ class TypeGrader implements GraderInterface
     {
         $mark=0;
         $right=0;
+        $grade=$answer['correct_answer'];
         if(isset($answer['student_answer'])){
             switch($answer['correct_answer']->type){
                 case 1 : //single
                     foreach($answer['correct_answer']->details as $detail){
+                        $detail->stu_ans=0;
+                        $detail->right=0;
                         if($detail->key == (int)$answer['student_answer'][0] && $detail->is_true==1){
                             $mark+=$detail->mark;
                             $right=1;
+                            $detail->right=$right;
                         }
+                        if($detail->key == (int)$answer['student_answer'][0])
+                            $detail->stu_ans=1;
                     }
                     break;
+
                 case 2 : // multi
                     foreach($answer['correct_answer']->details as $detail){
-                        for($i=0;$i<=count($answer['student_answer']);$i++){
-                            if($detail->key == $answer && $detail->is_true==1)
-                                $mark+=$detail->mark;
-                            
-                            if($mark < $answer['correct_answer']->total_mark)
-                                $mark=0;
-                        }
-                    }
-                    break;
-                case 3 : // part
-                    foreach($answer['correct_answer']->details as $detail){
+                        $detail->stu_ans=0;
+                        $detail->right=0;
                         for($i=0;$i<count($answer['student_answer']);$i++){
-                            if($detail->key == $answer['student_answer'][$i] && $detail->is_true==1)
-                                $mark+=$detail->mark;
+                            if($detail->key == $answer['student_answer'][$i] && $detail->is_true==1){
+                                if($detail->is_true == 1){
+                                    $mark+=$detail->mark;
+                                    $detail->right=1;
+                                }
+                                $detail->stu_ans=1;
+                            }
                         }
                     }
                     if($mark < $answer['correct_answer']->total_mark)
-                        $right=2; // part_answer
+                        $mark=0;
+                    if($mark == $answer['correct_answer']->total_mark)
+                        $right=1;
+                    break;
+
+                case 3 : // part
+                    foreach($answer['correct_answer']->details as $detail){
+                        $detail->stu_ans=0;
+                        $detail->right=0;
+                        for($i=0;$i<count($answer['student_answer']);$i++){
+                            if($detail->key == $answer['student_answer'][$i] && $detail->is_true==1){
+                                if($detail->is_true == 1){
+                                    $mark+=$detail->mark;
+                                    $detail->right=1;
+                                }
+                                $detail->stu_ans=1;
+                            }
+                        }
+                    }
+                    if($mark < $answer['correct_answer']->total_mark)
+                        $right=2;
                     if($mark == $answer['correct_answer']->total_mark)
                         $right=1;
                     break;
             }
         }
-        $grade['mark']=$mark;
-        $grade['right']=$right;
+        $grade->mark=$mark;
+        $grade->right=$right;
         return $grade;
     }
 
