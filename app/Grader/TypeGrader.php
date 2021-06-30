bce3561f872ccee6248e5207cf4c3c6fcf2b786f
Hend_Kagha <hend.genady@learnovia.com>
2021-06-30 14:15:24 +0200

    separate checks

--- app/Grader/TypeGrader.php
+++ app/Grader/TypeGrader.php
@@ -126,6 +126,7 @@ class TypeGrader implements GraderInterface
                 $ans->right=0;
                 $ans->grade=0;
                 foreach($ans as $key=>$aa){
+                    if(is_int($key))
                     foreach($answer['correct_answer']->mark[$key] as $mrk){
                         if($key === $aa){
                             $ans->right=1;
