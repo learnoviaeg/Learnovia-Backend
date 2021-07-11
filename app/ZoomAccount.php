780a8cb3526395f34926268b44f22719a2d3f566
Hend_Kagha <hend.genady@learnovia.com>
2021-07-11 15:01:17 +0200

    add new pass

--- app/ZoomAccount.php
+++ app/ZoomAccount.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ZoomAccount extends Model
 {
-    protected $fillable = ['user_id','jwt_token','api_key','api_secret','user_zoom_id','email'];
+    protected $fillable = ['user_id','jwt_token','api_key','api_secret','user_zoom_id','email','password'];
 
     public function user()
     {
