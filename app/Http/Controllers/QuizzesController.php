45b5eb19cccba5d799e1b48d2f50f00949ea132b
Hend_Kagha <hend.genady@learnovia.com>
2021-07-15 14:28:21 +0200

    allow_edit

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -214,7 +214,7 @@ class QuizzesController extends Controller
             // $quiz->Question()->attach($questionsIDs);
             // $quiz=Quiz::whereId($quiz->id)->with('Question.children')->get();
             
-            return HelperController::api_response_format(200, $quiz,__('messages.quiz.add'));
+            return HelperController::api_response_format(200,Quiz::find($quiz->id),__('messages.quiz.add'));
         // }
         return HelperController::api_response_format(200, null, __('messages.error.not_found'));
     }
