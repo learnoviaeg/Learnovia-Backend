b0cbfd29d505e6e95592877a8c2a2ef040365b5b
salma01 <salma.amr@learnovia.com>
2021-07-25 16:06:10 +0200

    debug

--- app/Listeners/RefreshGradeTreeListener.php
+++ app/Listeners/RefreshGradeTreeListener.php
@@ -29,22 +29,21 @@ class RefreshGradeTreeListener
      */
     public function handle(RefreshGradeTreeEvent $event)
     {             
-        if(!is_null($event->grade_category->calculation_type)){
-            foreach($event->grade_category->calculation_type as $calculation_type){
-                Log::debug('listener');
-                $calculator = resolve($calculation_type);
-                $grade = ($calculator->calculate($event->user , $event->grade_category));
-                UserGrader::updateOrCreate(
-                    ['item_id'=>$event->grade_category->id, 'item_type' => 'category', 'user_id' => $event->user->id],
-                    ['grade' =>  $grade]
-                );
+        if(is_null($event->grade_category->calculation_type)){
+            Log::debug($event);
+            throw new Exception('Missing calculation type');
+        }
+        foreach($event->grade_category->calculation_type as $calculation_type){
+            Log::debug('listener');
+            $calculator = resolve($calculation_type);
+            $grade = ($calculator->calculate($event->user , $event->grade_category));
+            UserGrader::updateOrCreate(
+                ['item_id'=>$event->grade_category->id, 'item_type' => 'category', 'user_id' => $event->user->id],
+                ['grade' =>  $grade]
+            );
 
-                Log::debug($event->grade_category->id .'  \   '.  $event->user->id . ' \ '. $grade);
+            Log::debug($event->grade_category->id .'  \   '.  $event->user->id . ' \ '. $grade);
 
-            }
-    }
-
-        
-        
+        }        
     }
 }
