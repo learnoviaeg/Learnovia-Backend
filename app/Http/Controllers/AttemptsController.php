2f6f6d698eed3689913c673f9c73ee15a89dcf08
Hend_Kagha <hend.genady@learnovia.com>
2021-07-25 10:05:30 +0200

    debugging

--- app/Http/Controllers/AttemptsController.php
+++ app/Http/Controllers/AttemptsController.php
@@ -141,6 +141,7 @@ class AttemptsController extends Controller
         $due_date=$attempt->quiz_lesson->due_date;
         $grade_feedback=$attempt->quiz_lesson->quiz->grade_feedback;
         $correct_feedback=$attempt->quiz_lesson->quiz->correct_feedback;
+        dd($correct_feedback);
         
         foreach($attempt->UserQuizAnswer as $one)
         {
