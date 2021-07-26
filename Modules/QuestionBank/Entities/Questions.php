85cfa8a25334e774d6eecc7fcc437923bad2915e
salma01 <salma.amr@learnovia.com>
2021-07-26 12:36:10 +0200

    mcq type

--- Modules/QuestionBank/Entities/Questions.php
+++ Modules/QuestionBank/Entities/Questions.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Questions extends Model
 {
-    protected $fillable = ['text','mark','parent','content','category_id','survey','question_type_id','question_category_id','course_id'];
+    protected $fillable = ['text','mark','parent','content','category_id','survey','question_type_id','question_category_id','course_id' , 'mcq_type'];
     protected $hidden = [
         'created_at', 'updated_at','course_segment_id','category_id'
     ];
