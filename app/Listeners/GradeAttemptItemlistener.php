a21ad57eaa770b063cdb6cf4035a08d08e4c56be
salma01 <salma.amr@learnovia.com>
2021-07-27 12:15:28 +0200

    fire grading methods event

--- app/Listeners/GradeAttemptItemlistener.php
+++ app/Listeners/GradeAttemptItemlistener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\UpdatedAttemptEvent;
+use App\Events\RefreshGradeTreeEvent;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\GradeItems;
