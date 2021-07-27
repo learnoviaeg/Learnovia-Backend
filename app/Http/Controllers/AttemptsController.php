e1698faa8ac97753299f7cbff6832a5a9df785d8
Hend_Kagha <hend.genady@learnovia.com>
2021-07-27 12:15:59 +0200

    get taken duration

--- app/Http/Controllers/AttemptsController.php
+++ app/Http/Controllers/AttemptsController.php
@@ -171,7 +171,7 @@ class AttemptsController extends Controller
 
                 $user_Attemp["open_time"]= $attem->open_time;
                 $user_Attemp["submit_time"]= $attem->submit_time;
-                $user_Attemp["taken_duration"]= $attem->open_time->diffInSeconds($attem->submit_time,false);
+                $user_Attemp["taken_duration"]= $attem->open_time->diffInSeconds($attem->submit_time,false)/60;
                 $user_Attemp['details']= UserQuiz::whereId($attem->id)->with('UserQuizAnswer.Question.children')->first();
 
                 $useranswerSubmitted = userQuizAnswer::where('user_quiz_id',$attem->id)->where('force_submit',null)->count();
