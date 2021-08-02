b1f2a5261b387afedc5be22919b286d8118f4416
salma01 <salma.amr@learnovia.com>
2021-08-02 09:16:35 +0200

    ongoing courses

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\CourseResource;
 use Illuminate\Support\Facades\Auth;
 use App\Repositories\ChainRepositoryInterface;
 use Carbon\Carbon;
@@ -68,57 +69,19 @@ class CoursesController extends Controller
             if($request->has('role_id')){
                 $enrolls->where('role_id',$request->role_id);
             }
-            $enrolls = $enrolls->with(['courseSegment.courses.attachment','levels'])->get()->groupBy(['course','level']);
-            foreach($enrolls as $course){
-                $levels=[];
-                $teacher = [];
-                foreach($course as $level){
-                    $teacher[] = User::whereIn('id',Enroll::where('role_id', '4')->where('course',  $level[0]->course)->where('level',$level[0]->level)
-                                                    ->pluck('user_id')
-                                )->with('attachment')->get(['id', 'username', 'firstname', 'lastname', 'picture']);
+            $enrolls = $enrolls->whereHas("courseSegment", function ($q) use ($request, $status) {
+                if($status =="ongoing")
+                    $q->where("end_date", '>' ,Carbon::now())->where("start_date", '<=' ,Carbon::now());
 
-                    $start_date = $level[0]->courseSegment->start_date;
-                    $end_date = $level[0]->courseSegment->end_date;
+                if($status =="future")
+                    $q->where("end_date", '>' ,Carbon::now())->where("start_date", '>' ,Carbon::now());
 
-                    if($status =="ongoing"){ // new route for  onoing courses in ative year api/course/ongoing
-                        if($level[0]->courseSegment->end_date > Carbon::now() && $level[0]->courseSegment->start_date <= Carbon::now()){
-                            $levels[] =  isset($level[0]->levels) ? $level[0]->levels->name : null;
-                            $temp_course = $level[0]->courseSegment->courses[0];
-                        }
-                    }
-                    if($status =="future"){// new route for  future courses in ative year api/course/future
-                        if($level[0]->courseSegment->end_date > Carbon::now() && $level[0]->courseSegment->start_date > Carbon::now()){ 
-                            $levels[] =  isset($level[0]->levels) ? $level[0]->levels->name : null;
-                            $temp_course = $level[0]->courseSegment->courses[0];     
-                        }
-                    }
-                    if($status =="past"){ // new route for  past courses in ative year api/course/past
-                        if($level[0]->courseSegment->end_date < Carbon::now() && $level[0]->courseSegment->start_date < Carbon::now()){ 
-                            $levels[] =  isset($level[0]->levels) ? $level[0]->levels->name : null;
-                            $temp_course = $level[0]->courseSegment->courses[0];     
-                        }
-                    }
-                }
-
-                if(!isset($temp_course))
-                    continue;
-
-                $user_courses->push([
-                    'id' => $temp_course->id ,
-                    'name' => $temp_course->name ,
-                    'short_name' => $temp_course->short_name ,
-                    'image' => isset($temp_course->image) ? $temp_course->attachment->path : null,
-                    'description' => $temp_course->description ,
-                    'mandatory' => $temp_course->mandatory == 1 ? true : false ,
-                    'level' => $levels,
-                    'teachers' => collect($teacher)->collapse()->unique()->values(),
-                    'start_date' => $start_date,
-                    'end_date' => $end_date,
-                    'progress' => round($temp_course->progress,2) ,
-                ]);
-
-                $temp_course = null;
-            }
+                if($status == "past")
+                    $q->where("end_date", '<' ,Carbon::now())->where("start_date", '<' ,Carbon::now());
+                })->with(['courses.attachment','levels',])->with(array('courseSegment.teachersEnroll.user' => function($query) {
+                    $query->addSelect(array('id', 'firstname', 'lastname', 'picture'));
+                }))->groupBy(['course','level'])->get();
+            return response()->json(['message' => __('messages.course.list'), 'body' => CourseResource::collection($enrolls)->paginate($paginate)], 200);
         }
 
         if($status == null){
@@ -166,7 +129,6 @@ class CoursesController extends Controller
                 });
             }
         }
-
         return response()->json(['message' => __('messages.course.list'), 'body' => $user_courses->paginate($paginate)], 200);
 
     }
