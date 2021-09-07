31abd7ffb0c48532e3802df807a49bb72b90eddc
salma01 <salma.amr@learnovia.com>
2021-09-07 16:15:45 +0200

    temp

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -320,7 +320,7 @@ class CoursesController extends Controller
                     if($request->old_lessons == 0){
                         $old_lessons = Lesson::where('course_id', $course);
                         $secondary_chains = SecondaryChain::where('group_id',$class)->whereIn('lesson_id',$old_lessons->get())->where('course_id',$course)->get()->pluck('lesson_id');
-                        $old_ids =  $old_lessons->where('id',$secondary_chains)->get()->pluck('id');
+                        $old_ids =  $old_lessons->whereIn('id',$secondary_chains)->get()->pluck('id');
                     }
                     $lessonsPerGroup = SecondaryChain::where('group_id',$class)->where('course_id',$request->template_id)->get()->pluck('lesson_id');
                     $new_lessons = Lesson::whereIn('id', $lessonsPerGroup)->get();
