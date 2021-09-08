2e45d161fda13652ca9b8a37054a3b0304ed29b0
salma01 <salma.amr@learnovia.com>
2021-09-08 10:23:53 +0200

    lesson Edit

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -319,20 +319,23 @@ class CoursesController extends Controller
                     $old_ids =  $old_lessons->get()->pluck('id');
                 }
                 foreach ($classes_of_course->classes as $class) {
+                    $shared_ids = [];
                     if($request->old_lessons == 0){
                         $secondary_chains = SecondaryChain::where('group_id',$class)->whereIn('lesson_id',$old_lessons->get())->where('course_id',$course)->delete();                            
                     }
                     $lessonsPerGroup = SecondaryChain::where('group_id',$class)->where('course_id',$request->template_id)->get()->pluck('lesson_id');
                     $new_lessons = Lesson::whereIn('id', $lessonsPerGroup)->get();
                     foreach($new_lessons as $lesson){
-                        if($lesson->shared_lesson == 1){
-                            lesson::firstOrcreate([
+                        if($lesson->shared_lesson == 1 ){
+                            $id = lesson::firstOrcreate([
                                 'name' => $lesson->name,
                                 'index' => $lesson->index,
                                 'shared_lesson' => $lesson->shared_lesson,
                                 'course_id' => $course,
                                 'shared_classes' => $lesson->getOriginal('shared_classes'),
                             ]);
+                        event(new LessonCreatedEvent(Lesson::find($id)));
+                        $shared_ids[] = $id;
                         }else{
                             lesson::create([
                                 'name' => $lesson->name,
@@ -346,7 +349,7 @@ class CoursesController extends Controller
             }
 
             if($request->old_lessons == 0){
-                Lesson::whereIn('id',$old_ids)->delete();
+                Lesson::whereIn('id',$old_ids)->whereNotIn('id',$shared_ids)->delete();
             }
         }
         return HelperController::api_response_format(200, null, __('messages.course.template'));
