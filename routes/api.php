8cf0062db9337d5155fedd9c67d8b9564bac61b8
Hend_Kagha <hend.genady@learnovia.com>
2021-09-13 13:51:33 +0200

    api of material

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 Route::get('/' , 'AuthController@site');
-Route::get('/materials/{id}', 'MaterialsController@show')->middleware(['auth:api','getauth','LastAction']);
+Route::get('/materials/{id}', 'MaterialsController@show')->middleware(['getauth','LastAction']);
 Route::get('/interactive/{id}', 'InterActiveController@show')->middleware(['getauth','LastAction']);
 
 //install all permissions and roles of system Route
