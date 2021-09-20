9afb81bf6abb0bef4a1b78fc82030ca6a7b2f83a
Hend_Genady <hend.genady@learnovia.com>
2021-09-20 13:59:36 +0200

    merge announcement

---
+++ bootstrap/cache/events.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'App\\Providers\\EventServiceProvider' => 
+  array (
+    'Illuminate\\Auth\\Events\\Registered' => 
+    array (
+      0 => 'Illuminate\\Auth\\Listeners\\SendEmailVerificationNotification',
+    ),
+    'App\\Events\\MassLogsEvent' => 
+    array (
+      0 => 'App\\Listeners\\MassLogsListener',
+    ),
+    'App\\Events\\GradeItemEvent' => 
+    array (
+      0 => 'App\\Listeners\\ItemDetailslistener',
+    ),
+    'App\\Events\\QuizAttemptEvent' => 
+    array (
+      0 => 'App\\Listeners\\AttemptItemlistener',
+    ),
+    'App\\Events\\RefreshGradeTreeEvent' => 
+    array (
+      0 => 'App\\Listeners\\RefreshGradeTreeListener',
+    ),
+    'App\\Events\\UpdatedAttemptEvent' => 
+    array (
+      0 => 'App\\Listeners\\FireAutoCorrectionEventListener',
+      1 => 'App\\Listeners\\GradeAttemptItemlistener',
+    ),
+    'App\\Events\\UpdatedQuizQuestionsEvent' => 
+    array (
+      0 => 'App\\Listeners\\UpdateQuizGradeListener',
+      1 => 'App\\Listeners\\UpdateTimelineListener',
+    ),
+    'App\\Events\\UserEnrolledEvent' => 
+    array (
+      0 => 'App\\Listeners\\AddUserGradersListener',
+    ),
+    'App\\Events\\CourseCreatedEvent' => 
+    array (
+      0 => 'App\\Listeners\\EnrollAdminListener',
+    ),
+    'App\\Events\\LessonCreatedEvent' => 
+    array (
+      0 => 'App\\Listeners\\AddSecondChainListener',
+    ),
+    'App\\Events\\ManualCorrectionEvent' => 
+    array (
+      0 => 'App\\Listeners\\GradeManualListener',
+    ),
+  ),
+);
