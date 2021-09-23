332013e6525dc7c4a8b0f183ca2dad151aa86d12
miirnamurad <48585377+miirnamurad@users.noreply.github.com>
2021-09-23 19:41:34 +0200

    Update ComponentsHelper.php

--- app/Helpers/ComponentsHelper.php
+++ app/Helpers/ComponentsHelper.php
@@ -21,12 +21,10 @@ class ComponentsHelper
 
     public function setCourse($course){
         $this->course = $course;
-        $this->getLessons();
     }
 
     public function setClass($class){
         $this->class = $class;
-        $this->getLessons();
     }
 
     public function setTeacher($teacher){
