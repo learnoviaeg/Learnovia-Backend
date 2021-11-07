0fb354c1197093b650963f2f641c415321b22577
Hend_Kagha <hend.genady@learnovia.com>
2021-11-07 09:27:34 +0200

    except admin

--- app/Http/Controllers/AttemptsController.php
+++ app/Http/Controllers/AttemptsController.php
@@ -339,8 +339,11 @@ class AttemptsController extends Controller
     {
         $attempt=UserQuiz::whereId($id)->with('UserQuizAnswer.Question','user','quiz_lesson')->first();
         // to prevent any user except auth to review this attempt
-        if($attempt->user_id != Auth::id())
-            return HelperController::api_response_format(201, __('messages.error.data_invalid'));
+
+        if(Auth::user()->can('site/course/student')){
+            if($attempt->user_id != Auth::id())
+                return HelperController::api_response_format(201, __('messages.error.data_invalid'));
+        }
 
         $due_date=$attempt->quiz_lesson->due_date;
         $grade_feedback=$attempt->quiz_lesson->quiz->grade_feedback;
