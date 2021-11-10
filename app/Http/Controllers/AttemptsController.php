2827ddbcd00260988ca6c02a27e20f7ac9d00d07
Yasser74 <Yasser74>
2021-11-10 11:29:00 +0200

    edit grade of attempts

--- app/Http/Controllers/AttemptsController.php
+++ app/Http/Controllers/AttemptsController.php
@@ -534,6 +534,7 @@ class AttemptsController extends Controller
             'classes.*' => 'exists:classes,id',
         ]);
         $quiz_lesson = QuizLesson::where('quiz_id', $request->quiz_id)->where('lesson_id', $request->lesson_id)->first();
+        $gradeCategory = GradeCategory::where('id' , $quiz_lesson->grade_category_id)->pluck('id');
         $course = Course::where('id' ,$quiz_lesson->lesson->course_id )->first();
         $level = $course->level;
         $course = [$quiz_lesson->lesson->course_id];
@@ -545,6 +546,9 @@ class AttemptsController extends Controller
         $notGraded = array();
         $allSubmitted = array();
         foreach ($enrolls->pluck('user_id') as $user_id){
+            $grade = UserGrader::where('user_id' , $user_id)
+            ->where('item_id' , $gradeCategory)
+            ->where('item_type' ,'category')->pluck('grade');
             $user = User::find($user_id);
             if($user == null){
                 unset($user);
@@ -570,7 +574,7 @@ class AttemptsController extends Controller
             $user_Attemp['fullname'] = $user->fullname;
             $user_Attemp['level'] = $level->name;
             $user_Attemp["status"] = $attems->status;
-            $user_Attemp["grade"] = $attems->grade;
+            $user_Attemp["grade"] = $grade;
             $user_Attemp["attempt_index"] = $attems->attempt_index;
             $user_Attemp["last_att_date"] = $attems->submit_time;
             $Submitted[] = $user_Attemp;
