d7290aab2d726301da0982ecba947ff364841618
Hend_Kagha <hend.genady@learnovia.com>
2021-11-17 09:08:19 +0200

    an

--- Modules/QuestionBank/Observers/QuizLessonObserver.php
+++ Modules/QuestionBank/Observers/QuizLessonObserver.php
@@ -61,7 +61,9 @@ class QuizLessonObserver
                     UserGrader::firstOrCreate([
                         'user_id'   => $student,
                         'item_type' => 'Category',
-                        'item_id'   => $categoryOfQuiz->id,
+                        'item_id'   => $categoryOfQuiz->id
+                    ],
+                    [
                         'grade'     => null
                     ]);
                 };  
