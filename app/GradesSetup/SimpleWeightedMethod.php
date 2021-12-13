0d5884e6ee26e216433678a88cb64184feefe113
salma01 <salma.amr@learnovia.com>
2021-12-13 10:05:08 +0200

    use user grader

--- app/GradesSetup/SimpleWeightedMethod.php
+++ app/GradesSetup/SimpleWeightedMethod.php
@@ -41,7 +41,7 @@ class SimpleWeightedMethod implements GradeSetupInterface
         $max_total = 0;
         foreach($grade_category->categories_items as $child){
             $user_mark = userGrader::select('grade')->where('user_id', $user->id)->where('item_id',$child->id)->where('item_type','category')->first();
-            $total_user_mark += $grade_per_item;
+            $total_user_mark += $user_mark->grade;
             $max_total += $child->max;
         }
         $grade = ($total_user_mark/$max_total) *($grade_category->max);
