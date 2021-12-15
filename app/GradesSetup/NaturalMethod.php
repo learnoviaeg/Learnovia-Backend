f12c7749dd63ac1e4d1a65bac4a974fb93801d9d
salma01 <salma.amr@learnovia.com>
2021-12-15 09:50:54 +0200

    validate weight

--- app/GradesSetup/NaturalMethod.php
+++ app/GradesSetup/NaturalMethod.php
@@ -65,6 +65,6 @@ class NaturalMethod implements GradeSetupInterface
         $adjusted_children = $grade_category->categories_items()->where('weight_adjust', 1)->count();
         $all_children = $grade_category->categories_items()->count();
         if($adjusted_children == $all_children)
-            $grade_category->GradeItems()->update(['weight_adjust' => 0]);
+            $grade_category->categories_items()->update(['weight_adjust' => 0]);
     }
 }
