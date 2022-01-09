7754f0fcfbadfc28ccc7e0155a9464ed5632d278
Hend_Kagha <hend.genady@learnovia.com>
2022-01-09 17:19:01 +0200

    permission_levels

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -312,11 +312,29 @@ class SettingsController extends Controller
         return response()->json(['message' => __('messages.logo.update'), 'body' => $attachment], 200);
     }
 
-    public function editor()
+    public function editor(Request $request)
     {
-        return view('editor');
+        $objEditor= [
+            'selector' => 'textarea#open-source-plugins',
+            'plugins' => 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+            'menubar' => 'file edit view insert format tools table help',
+            'toolbar' => 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+            'toolbar_sticky' => true,
+            'autosave_ask_before_unload' => true,
+            'autosave_interval' => '30s',
+            'autosave_prefix' => '{path}{query}-{id}-',
+            'autosave_restore_when_empty' => false,
+            'autosave_retention' => '2m',
+            'image_advtab' => true,
+            // 'external_plugins' => [
+            //   'tiny_mce_wiris' => 'https://www.wiris.net/demo/plugins/tiny_mce/plugin.js'
+            // ],
+            'importcss_append' => true,
+            'content_style' => 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px;height:100% }'
+        ];
+        return view('editor', compact('objEditor'));
     }
-    
+
     public function setPermissionLevel(Request $request)
     {
         $request->validate([
