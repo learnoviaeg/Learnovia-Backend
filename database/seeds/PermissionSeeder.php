334fe85537d91749ab50af10c10bdefa81583c70
Hend_Kagha <hend.genady@learnovia.com>
2022-01-10 10:24:02 +0200

    route in attendance

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -421,7 +421,7 @@ class PermissionSeeder extends Seeder
         Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/add', 'title' => 'Create Attendnace']);
         Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/delete', 'title' => 'Delete Attendance']);
         Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/edit', 'title' => 'Edit Attendance']);
-        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/get', 'title' => 'Get Attendances']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/viewAllAttendance', 'title' => 'Get Attendances']);
 
         Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/add-log', 'title' => 'Take attendnace']);
         Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'attendance/get-daily', 'title' => 'Daily','dashboard' => 1,'icon'=> 'Attendance']);
