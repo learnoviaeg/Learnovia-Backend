4ea1cb75ec569bf37e5553d092e48d9026c4222c
Hend_Kagha <hend.genady@learnovia.com>
2022-01-19 10:19:30 +0200

    filter of sessions

--- app/Http/Controllers/AttendanceSessionController.php
+++ app/Http/Controllers/AttendanceSessionController.php
@@ -12,10 +12,10 @@ class AttendanceSessionController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware(['permission:attendance/add-session'],   ['only' => ['store']]);
-        // $this->middleware(['permission:attendance/get-session'],   ['only' => ['index','show']]);
-        // $this->middleware(['permission:attendance/delete-session'],   ['only' => ['destroy']]);
-        // $this->middleware(['permission:attendance/edit-session'],   ['only' => ['update']]);
+        $this->middleware(['permission:attendance/add-session'],   ['only' => ['store']]);
+        $this->middleware(['permission:attendance/get-session'],   ['only' => ['index','show']]);
+        $this->middleware(['permission:attendance/delete-session'],   ['only' => ['destroy']]);
+        $this->middleware(['permission:attendance/edit-session'],   ['only' => ['update']]);
     }
 
     /**
