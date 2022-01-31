c35a83cac5571844c931f8424c696019eb66868f
salma01 <salma.amr@learnovia.com>
2022-01-31 15:14:20 +0200

    manara report

--- app/Http/Controllers/ReportCardsController.php
+++ app/Http/Controllers/ReportCardsController.php
@@ -13,7 +13,7 @@ class ReportCardsController extends Controller
     {
         $this->chain = $chain;
         $this->middleware('auth');
-        $this->middleware(['permission:report_card/haramain'],   ['only' => ['haramainReport']]);
+        $this->middleware(['permission:report_card/mfisg|report_card/mfisb'],   ['only' => ['manaraReport']]);
     }
 
     public function haramainReport(Request $request)
@@ -69,6 +69,59 @@ class ReportCardsController extends Controller
         $allowed_levels=Permission::where('name','report_card/forsan')->pluck('allowed_levels')->first();
         $allowed_levels=json_decode($allowed_levels);
 
+        $student_levels = Enroll::where('user_id',$request->user_id)->pluck('level')->toArray();
+        $check=(array_intersect($allowed_levels, $student_levels));
+
+        if(count($check) == 0)
+            return response()->json(['message' => 'You are not allowed to see report card', 'body' => null ], 200);
+
+        $grade_category_callback = function ($qu) use ($request ) {
+            $qu->whereNull('parent')
+            ->with(['Children.userGrades' => function($query) use ($request){
+                $query->where("user_id", $request->user_id);
+            },'GradeItems.userGrades' => function($query) use ($request){
+                $query->where("user_id", $request->user_id);
+            },'userGrades' => function($query) use ($request){
+                $query->where("user_id", $request->user_id);
+            }]); 
+        };
+
+        $course_callback = function ($qu) use ($request ) {
+            $qu->Where(function ($query) {
+                $query->where('name', 'LIKE' , "%Grades%")
+                      ->orWhere('name','LIKE' , "%درجات%"); 
+            });     
+        };
+
+        $callback = function ($qu) use ($request , $course_callback , $grade_category_callback) {
+            $qu->where('role_id', 3);
+            $qu->whereHas('courses' , $course_callback)
+                ->with(['courses' => $course_callback]); 
+            $qu->whereHas('courses.gradeCategory' , $grade_category_callback)
+                ->with(['courses.gradeCategory' => $grade_category_callback]); 
+        };
+
+        $result = User::whereId($request->user_id)->whereHas('enroll' , $callback)
+                        ->with(['enroll' => $callback , 'enroll.levels' , 'enroll.type'])->first();
+
+        return response()->json(['message' => null, 'body' => $result ], 200);
+    }
+
+    public function manaraReport(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+        ]);
+        $GLOBALS['user_id'] = $request->user_id;
+        $user = User::find($request->user_id);
+
+        if($user->can('report_card/mfisg'))
+            $allowed_levels=Permission::where('name','report_card/mfisg')->pluck('allowed_levels')->first();
+        
+        if($user->can('report_card/mfisb'))
+            $allowed_levels=Permission::where('name','report_card/mfisb')->pluck('allowed_levels')->first();
+
+        $allowed_levels=json_decode($allowed_levels);
         $student_levels = Enroll::where('user_id',$request->user_id)->pluck('level')->toArray();
         $check=(array_intersect($allowed_levels, $student_levels));
 
