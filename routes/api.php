0e67944f11dae3e6cb5af68a05493569ec36e8c7
salma01 <salma.amr@learnovia.com>
2022-02-07 11:44:00 +0200

    fgls print all

--- routes/api.php
+++ routes/api.php
@@ -576,6 +576,7 @@ Route::group(['prefix' => 'schools-report', 'middleware' => ['auth:api']], funct
     Route::post('manara-all', 'ReportCardsController@manaraReportAll');
     Route::post('haramain-all', 'ReportCardsController@haramaninReportAll');
     Route::post('forsan-all', 'ReportCardsController@forsanReportAll');
+    Route::post('fgl-all', 'ReportCardsController@fglsReportAll');
 });
 
 //script for front-end editor
