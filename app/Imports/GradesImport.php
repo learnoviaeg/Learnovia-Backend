04ab22604669b4c5ea10e3262e1f7bbb1641aaad
salma01 <salma.amr@learnovia.com>
2022-02-09 16:19:46 +0200

    grades import

--- app/Imports/GradesImport.php
+++ app/Imports/GradesImport.php
@@ -33,8 +33,21 @@ class GradesImport implements  ToModel, WithHeadingRow
         foreach($array as $key => $item){
             $item_id = substr($key,strrpos($key,"_")+1);
             $user = User::select('id')->where('username' , $row['username'])->first();
+
+            if(is_string($item)){
+                if(GradeCategory::find($item_id)->scale_id == null)
+                    dd('Cannot add scale for this item');
+
+                GradeCategory::find($item_id)->scale->details()->where('name', $item);
+                    $req[0]['scale_id'] = GradeCategory::find($item_id)->scale->details()->where('evaluation', $item)->first()->id;
+            }
+            else{
+                if(GradeCategory::find($item_id)->scale_id != null)
+                    dd('Cannot add grade for this item');
+                $req[0]['grade'] = $item;
+            }
+
             $req[0]['item_id'] = $item_id;
-            $req[0]['grade'] = $item;
             $req[0]['user_id'] = $user->id;
             $request = new Request([
                 'user' => $req,
