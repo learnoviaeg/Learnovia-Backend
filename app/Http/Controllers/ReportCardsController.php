c40c03ea47d8548f94bd6c3d2f56c4806e3a3a26
salma01 <salma.amr@learnovia.com>
2022-02-28 10:51:57 +0200

    fgls prep3

--- app/Http/Controllers/ReportCardsController.php
+++ app/Http/Controllers/ReportCardsController.php
@@ -422,4 +422,54 @@ class ReportCardsController extends Controller
         }
         return response()->json(['message' => null, 'body' => $result_collection ], 200);
     }
+
+
+
+    public function fglPrep3Report(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $allowed_levels=Permission::where('name','report_card/fgls')->pluck('allowed_levels')->first();
+        $allowed_levels=json_decode($allowed_levels);
+
+        $student_levels = Enroll::where('user_id',$request->user_id)->pluck('level')->toArray();
+        $check=(array_intersect($allowed_levels, $student_levels));
+
+        if(count($check) == 0)
+            return response()->json(['message' => 'You are not allowed to see report card', 'body' => null ], 200);
+        
+        $GLOBALS['user_id'] = $request->user_id;
+        $grade_category_callback = function ($qu) use ($request ) {
+            $qu->whereNull('parent')
+            ->with(['Children.userGrades' => function($query) use ($request){
+                $query->where("user_id", $request->user_id);
+            },'GradeItems.userGrades' => function($query) use ($request){
+                $query->where("user_id", $request->user_id);
+            },'userGrades' => function($query) use ($request){
+                $query->where("user_id", $request->user_id);
+            }]); 
+        };
+
+        $course_callback = function ($qu) use ($request ) {
+            $qu->Where(function ($query) {
+                $query->where('name', 'LIKE' , "%Grades%")
+                      ->orWhere('name','LIKE' , "%درجات%");
+            });     
+        };
+
+        $callback = function ($qu) use ($request , $course_callback , $grade_category_callback) {
+            $qu->where('role_id', 3);
+            $qu->whereHas('courses' , $course_callback)
+                ->with(['courses' => $course_callback]); 
+            $qu->whereHas('courses.gradeCategory' , $grade_category_callback)
+                ->with(['courses.gradeCategory' => $grade_category_callback]); 
+        };
+
+        $result = User::whereId($request->user_id)->whereHas('enroll' , $callback)
+                        ->with(['enroll' => $callback , 'enroll.levels' , 'enroll.type'])->first();
+
+        return response()->json(['message' => null, 'body' => $result ], 200);
+    }
 }
