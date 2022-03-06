2aa39faf9ee405daeca0bbad81393d417f4acaca
salma01 <salma.amr@learnovia.com>
2022-03-06 10:43:36 +0200

    page update in material

--- Modules/Page/Observers/PageObserver.php
+++ Modules/Page/Observers/PageObserver.php
@@ -3,10 +3,29 @@
 namespace Modules\Page\Observers;
 
 use App\LessonComponent;
-use Modules\Page\Entities\PageLesson;
+use Modules\Page\Entities\Page;
+use App\Material;
+use App\Lesson;
+use App\SecondaryChain;
 
 class PageObserver
 {
+
+        /**
+     * Handle the page "updated" event.
+     *
+     * @param  \App\Page  $page
+     * @return void
+     */
+    public function updated(Page $page)
+    {
+        Material::where('item_id',$page->id)->where('type' , 'page')
+        ->update([
+            'name' => $page->title,
+        ]);
+
+    }
+
     public function deleted(PageLesson $lesson)
     {
         LessonComponent::where('comp_id',$lesson->page_id)->where('lesson_id',$lesson->lesson_id)
