5a1ba5c34715df44e68c0d2ce62b6abed9628a2a
salma01 <salma.amr@learnovia.com>
2022-03-07 16:20:22 +0200

    show all levels and classes

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -44,13 +44,13 @@ class LevelController extends Controller
         if($request->filled('search'))
             $levels->where('name', 'LIKE' , "%$request->search%");
 
+        if($request->filled('filter') && $request->filter == 'all' && $request->user()->can('site/show-all-courses'))
+            return HelperController::api_response_format(200, $levels->paginate(HelperController::GetPaginate($request)), __('messages.level.list'));
+
         $enrolls = $this->chain->getEnrollsByManyChain($request);
 
         if(!$request->has('user_id'))
             $enrolls->where('user_id',Auth::id());
-        
-        if($request->filled('filter') && $request->filter == 'all' && $request->user()->can('site/show-all-courses'))
-            return HelperController::api_response_format(200, $levels->paginate(HelperController::GetPaginate($request)), __('messages.level.list'));
 
         $levels->whereIn('id',$enrolls->pluck('level'));
         return HelperController::api_response_format(200, $levels->paginate(HelperController::GetPaginate($request)), __('messages.level.list'));
