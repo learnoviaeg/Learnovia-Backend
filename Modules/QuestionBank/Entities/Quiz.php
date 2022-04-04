2882e871bf9253045ccfe3ab5ef0166816b216ee
ahmed2mm <ahmed2mm@example.com>
2022-04-04 14:21:37 +0200

    quiz and quizlesson trait log added

--- Modules/QuestionBank/Entities/Quiz.php
+++ Modules/QuestionBank/Entities/Quiz.php
@@ -3,9 +3,15 @@
 namespace Modules\QuestionBank\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Auditable;
+use App\Lesson as Lessonmodel;
+use App\AuditLog;
+use Modules\QuestionBank\Entities\QuizLesson;
 
 class quiz extends Model
 {
+    use Auditable;
+
     protected $fillable = ['name','course_id','is_graded','duration','created_by' , 'shuffle','grade_feedback', 'draft', 'correct_feedback','allow_edit','restricted'];
     protected $hidden = [
         'created_at', 'updated_at'
@@ -66,4 +72,60 @@ class quiz extends Model
             return True;
         return False;
     }
+
+      // start function get name and value f attribute
+    public static function get_year_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_type_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_level_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_class_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_segment_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_course_name($old, $new)
+    {
+        // get lessons sent with with create quiz request 
+        // get course id that thses lessons belongs to 
+        // these lessons is under one course 
+        // save this course into audit_log
+        $lessons_id   = QuizLesson::where('quiz_id', $new->id)->pluck('lesson_id');
+        if (count($lessons_id) <= 0) {
+            $course_id = null;
+        }else{
+            $course_id[]  = Lessonmodel::whereIn('id', $lessons_id)->first()->course_id;
+            $audit_log_quiz_course_id = AuditLog::where(['subject_type' => 'quiz', 'subject_id' => $new->id])->first();
+            $audit_log_quiz_course_id->update([
+                'course_id' => $course_id
+            ]);
+        }
+        return $course_id;
+    }
+    // end function get name and value attribute
 }
--- Modules/QuestionBank/Entities/QuizLesson.php
+++ Modules/QuestionBank/Entities/QuizLesson.php
@@ -11,9 +11,14 @@ use Modules\QuestionBank\Entities\QuizOverride;
 use App\UserSeen;
 use App\SystemSetting;
 use App\UserGrader;
+use App\Traits\Auditable;
+use App\Lesson as Lessonmodel;
+use App\AuditLog;
 
 class QuizLesson extends Model
 {
+    use Auditable;
+
     protected $fillable = [
         'quiz_id',
         'lesson_id',
@@ -167,5 +172,53 @@ class QuizLesson extends Model
         parent::boot();
         static::addGlobalScope(new OverrideQuizScope);
     }
+
+    // start function get name and value f attribute
+    public static function get_year_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_type_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_level_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_class_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_segment_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_course_name($old, $new)
+    {
+        $lessons_id   = QuizLesson::where('quiz_id', $new->quiz_id)->pluck('lesson_id');
+        $course_id[]  = Lessonmodel::whereIn('id', $lessons_id)->first()->course_id;
+        $audit_log_quiz_course_id = AuditLog::where(['subject_type' => 'quiz', 'subject_id' => $new->quiz_id])->first();
+        $audit_log_quiz_course_id->update([
+            'course_id' => $course_id
+        ]);
+        return $course_id;
+    }
+    // end function get name and value attribute
 }
 
