4ef27b190dfbd12fd86b7a7154a54168c89f1b77
ahmed2mm <ahmed2mm@example.com>
2022-04-07 13:15:35 +0200

    page and pagelesson added trait log

--- Modules/Page/Entities/Page.php
+++ Modules/Page/Entities/Page.php
@@ -3,9 +3,15 @@
 namespace Modules\Page\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Auditable;
+use App\Lesson as Lessonmodel;
+use App\AuditLog;
+use Modules\Page\Entities\pageLesson;
 
 class page extends Model
 {
+    use Auditable;
+    
     protected $fillable = ['title', 'content', 'visible'];
     public function Lesson()
     {
@@ -30,4 +36,56 @@ class page extends Model
     {
         return $this->hasManyThrough('App\Lesson' ,'Modules\Page\Entities\pageLesson', 'page_id' , 'id' , 'id' , 'id' );
     }
+
+    // start function get name and value f attribute
+    public static function get_year_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_type_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_level_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_class_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_segment_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_course_name($old, $new)
+    {
+        $lessons_id   = pageLesson::where('page_id', $new->id)->pluck('lesson_id');
+        if (count($lessons_id) <= 0) {
+            $course_id = null;
+        }else{
+            $course_id[]  = Lessonmodel::whereIn('id', $lessons_id)->first()->course_id;
+            $audit_log_quiz_course_id = AuditLog::where(['subject_type' => 'page', 'subject_id' => $new->id])->first();
+            $audit_log_quiz_course_id->update([
+                'course_id' => $course_id
+            ]);
+        }
+        return $course_id;
+    }
+    // end function get name and value attribute
 }
--- Modules/Page/Entities/PageLesson.php
+++ Modules/Page/Entities/PageLesson.php
@@ -3,8 +3,61 @@
 namespace Modules\Page\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Auditable;
+use App\Lesson as Lessonmodel;
+use App\AuditLog;
 
 class pageLesson extends Model
 {
+	use Auditable;
+
     protected $fillable = ['page_id','lesson_id','visible' , 'publish_date'];
+
+    // start function get name and value f attribute
+    public static function get_year_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_type_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_level_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_class_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_segment_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_course_name($old, $new)
+    {
+        $lessons_id   = pageLesson::where('page_id', $new->page_id)->pluck('lesson_id');
+        $course_id[]  = Lessonmodel::whereIn('id', $lessons_id)->first()->course_id;
+        $audit_log_quiz_course_id = AuditLog::where(['subject_type' => 'page', 'subject_id' => $new->page_id])->first();
+        $audit_log_quiz_course_id->update([
+            'course_id' => $course_id
+        ]);
+        return $course_id;
+    }
+    // end function get name and value attribute
 }
