3d223f0b453d50737daa3e9903485cf9d02cc5ec
Hend_Kagha <hend.genady@learnovia.com>
2022-04-13 12:57:21 +0200

    translation permissions

--- database/seeds/TranslationSeeder.php
+++ database/seeds/TranslationSeeder.php
@@ -36,6 +36,7 @@ class TranslationSeeder extends Seeder
         Dictionary::firstOrCreate(['key' => 'report_card/forsan/monthly', 'language' => 1, 'value' => 'Forsan Monthly Report']);
         Dictionary::firstOrCreate(['key' => 'report_card/forsan/monthly/printAll', 'language' => 1, 'value' => 'Print all Monthly']);
         Dictionary::firstOrCreate(['key' => 'report_card/mfis/manara-girls/monthly/printAll', 'language' => 1, 'value' => 'Print all Monthly']);
+        Dictionary::firstOrCreate(['key' => 'report_card/mfis/mfisg-monthly', 'language' => 1, 'value' => 'Monthly Report']);
         Dictionary::firstOrCreate(['key' => 'reports/course_progress', 'language' => 1, 'value' => 'Course progress report']);
         Dictionary::firstOrCreate(['key' => 'report_card/fgls/final', 'language' => 1, 'value' => 'Final Report']);
         Dictionary::firstOrCreate(['key' => 'report_card/fgls/all-final', 'language' => 1, 'value' => 'All Final Reports']);
@@ -43,6 +44,7 @@ class TranslationSeeder extends Seeder
         Dictionary::firstOrCreate(['key' => 'report_card/forsan/monthly', 'language' => 2, 'value' => 'تقرير الدرجات الشهري']);
         Dictionary::firstOrCreate(['key' => 'report_card/forsan/monthly/printAll', 'language' => 2, 'value' => 'جميع التقارير الشهرية']);
         Dictionary::firstOrCreate(['key' => 'report_card/mfis/manara-girls/monthly/printAll', 'language' => 2, 'value' => 'جميع التقارير الشهرية']);
+        Dictionary::firstOrCreate(['key' => 'report_card/mfis/mfisg-monthly', 'language' => 2, 'value' => 'التقرير الشهري']);
         Dictionary::firstOrCreate(['key' => 'reports/course_progress', 'language' => 2, 'value' => 'تقرير المتابعه']);
         Dictionary::firstOrCreate(['key' => 'report_card/fgls/final', 'language' => 2, 'value' => 'الشهادة']);
         Dictionary::firstOrCreate(['key' => 'report_card/fgls/all-final', 'language' => 2, 'value' => 'جميع الشهادات']);
