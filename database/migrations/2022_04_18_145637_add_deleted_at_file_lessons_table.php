22c4e0e013349194ae52935c888dd24bade238e6
ahmed2mm <ahmed2mm@example.com>
2022-04-19 09:49:38 +0200

    added soft delete material modules

--- database/migrations/2022_04_18_145637_add_deleted_at_file_lessons_table.php
+++ database/migrations/2022_04_18_145637_add_deleted_at_file_lessons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDeletedAtFileLessonsTable extends Migration
+class AddDeletedAtFilesTable extends Migration
 {
     /**
      * Run the migrations.
