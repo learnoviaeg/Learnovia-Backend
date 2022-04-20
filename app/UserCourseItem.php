34da353c380703c79774194f5972f5d58d2f86f7
ahmed2mm <ahmed2mm@example.com>
2022-04-20 11:37:17 +0200

    added log to courseitem and usercourseitem

--- app/UserCourseItem.php
+++ app/UserCourseItem.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Auditable;
 
 class UserCourseItem extends Model
 {
+	use Auditable;
+
     protected $fillable = ['user_id', 'course_item_id', 'can_view'];
 
     public function courseItem(){
@@ -16,4 +19,45 @@ class UserCourseItem extends Model
         return $this->belongsTo('App\User', 'user_id', 'id');
     }
 
+    // start function get name and value f attribute
+    public static function get_year_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_type_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_level_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_class_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_segment_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
+
+    // start function get name and value f attribute
+    public static function get_course_name($old, $new)
+    {
+        return null;
+    }
+    // end function get name and value attribute
 }
