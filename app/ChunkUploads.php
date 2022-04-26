5b2c2b34618962b4eaa9fa1cad28d9473cf09cb2
salma01 <salma.amr@learnovia.com>
2022-04-26 12:29:58 +0200

    chunks upload

--- app/ChunkUploads.php
+++ app/ChunkUploads.php
@@ -3,14 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ChunkUploads extends Model
 {
-    protected $fillable = ['name','data','path', 'uploaded'];
+    protected $fillable = ['name','data','path', 'uploaded' , 'type'];
     protected $hidden = ['created_at','updated_at'];
 
   public function getPathAttribute() {
       return url(Storage::url($this->attributes['path']));
     }
-    
+
 }
