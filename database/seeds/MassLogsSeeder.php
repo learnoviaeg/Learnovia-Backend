398dc360e829eb17363dd9bbf5d14329f14a2cf0
Hend_Kagha <hend.genady@learnovia.com>
2022-05-10 10:38:53 +0200

    massive data

--- database/seeds/MassLogsSeeder.php
+++ database/seeds/MassLogsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\AuditLog;
 
 class MassLogsSeeder extends Seeder
 {
@@ -13,7 +14,7 @@ class MassLogsSeeder extends Seeder
     {
         for($i=3; $i>=0; $i++)
         {
-            AuditLog:: insert([
+            AuditLog:: create([
                 'action' => 'updated',
                 'subject_id' => 16 ,
                 'subject_type' => 'quiz',
@@ -21,13 +22,13 @@ class MassLogsSeeder extends Seeder
                 'properties' => '{"id":16,"name":"Ahmed Edited 0'.$i.'","is_graded":"0","duration":39660,"created_by":1,"shuffle":"No Shuffle","feedback":1,"course_id":5,"draft":0,"grade_feedback":"After due_date","correct_feedback":"After due_date","allow_edit":true,"restricted":false,"deleted_at":null}',
                 'before' => '{"id":16,"name":"Ahmed Edied 0'. ($i-1) . '","is_graded":0,"duration":39660,"created_by":1,"shuffle":"No Shuffle","created_at":"2022-05-08 13:22:35","updated_at":"2022-05-08 15:59:29","feedback":1,"course_id":5,"draft":0,"grade_feedback":"After due_date","correct_feedback":"After due_date","allow_edit":1,"restricted":0,"deleted_at":null}',
                 'host' => request()->ip() ?? null,
-                'year_id' => 1,
-                'type_id' => 1,
-                'level_id => 1',
+                'year_id' => [2],
+                'type_id' => [1],
+                'level_id' => [1],
                 'class_id' => [1],
-                'segment_id' => 1, 
-                'course_id' => 5,
-                'role_id' => 1, 
+                'segment_id' => [1], 
+                'course_id' => [5],
+                'role_id' => [1], 
                 'notes' => 'script',
             ]);
         }
