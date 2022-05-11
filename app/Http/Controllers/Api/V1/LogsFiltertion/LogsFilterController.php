c65a02431d4c969f1304794a27ac268c8396df4a
ahmed2mm <ahmed2mm@example.com>
2022-05-11 15:22:10 +0200

    simple paginate done

--- app/Http/Controllers/Api/V1/LogsFiltertion/LogsFilterController.php
+++ app/Http/Controllers/Api/V1/LogsFiltertion/LogsFilterController.php
@@ -126,14 +126,14 @@ class LogsFilterController extends Controller
 	    		$data = $data->where($key, $value);
 	    	}
 	    }
-	    $vv = $data->count();
+	    // $vv = $data->count();
 
-	    $collect_data = $data->skip($skip)->take($limit)->select('id', 'action','subject_type', 'subject_id', 'user_id', 'created_at', 'host', 'hole_description', 'item_name', 'item_id')->orderBy('id', 'DESC');
-	    $collection = $collect_data->simplePaginate($pagination);
-	    // $collection->count();
+	    $collection = $data->skip($skip)->take($limit)->select('id', 'action','subject_type', 'subject_id', 'user_id', 'created_at', 'host', 'hole_description', 'item_name', 'item_id')->orderBy('id', 'DESC')
+	                    ->simplePaginate($pagination);
+	    //$collection   = $collect_data->paginate(Paginate::GetPaginate($request));
 	    $collect_data = LogsFilterResource::collection($collection);
 
-	    return response()->json(['total' => $vv, 'data' => $collection, 'status_code' => 200], 200);
+	    return response()->json(['data' => $collection, 'status_code' => 200], 200);
     }
 
     // no chain filter selected
