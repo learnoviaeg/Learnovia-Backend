4393c024f585b507d8077e82e5f06ad2519d9226
haythamhappy <haytham@happyhuman.com>
2022-06-02 10:51:47 +0200

    Update FilesController.php

--- Modules/UploadFiles/Http/Controllers/FilesController.php
+++ Modules/UploadFiles/Http/Controllers/FilesController.php
@@ -482,10 +482,11 @@ class FilesController extends Controller
 
         
         $lesson = Lesson::find($request->lesson_id);
+        print_r($lesson);
         // $courseID = CourseSegment::where('id', $lesson->course_segment_id)->pluck('course_id')->first();
         LastAction::lastActionInCourse($lesson->course_id);
         $target_file = File::find($request->fileID);
-        print_r($target_file);
+
         $target_file->delete();
         $file   = FileLesson::where('file_id', $request->fileID)->where('lesson_id', $request->lesson_id)->first();
         $file->delete();
