84f3b67d1f0e1f272f9e3f15d8253612ec139a04
Hend_Kagha <hend.genady@learnovia.com>
2022-07-04 13:16:42 +0200

    statistics

--- app/Http/Controllers/BloomCategoryController.php
+++ app/Http/Controllers/BloomCategoryController.php
@@ -109,10 +109,10 @@ class BloomCategoryController extends Controller
             $qu->where('quiz_id', $request->quiz_id);  
         };
 
-        $bloom = BloomCategory::select('id','name as bloom_name')
-        ->withCount(['questions as count ' => function($query) use ($request, $quiz_question_callback){
-            $query->whereHas('quizQuestion' , $quiz_question_callback)->with(['quizQuestion' => $quiz_question_callback]);}
-        ]);
+        $bloom = BloomCategory::select('id','name as bloom_name')->where('current',1)
+            ->withCount(['questions' => function($query) use ($request, $quiz_question_callback){
+                $query->whereHas('quizQuestion' , $quiz_question_callback)->with(['quizQuestion' => $quiz_question_callback]);}
+            ]);
 
         $questionAnswers=array();
         foreach( $attemptss->cursor() as $att)
@@ -182,14 +182,7 @@ class BloomCategoryController extends Controller
                     $daragat+=$answer->user_grade;
             }
             // $a l result
-            // $categories=BloomCategory::where('current',1)->pluck('name');
-            // // return $categories;
-            // foreach($categories as $category){
-            //     if($category == $key)
             $a[$key]=round($daragat/$cc,2);
-            //     else
-            //         $a[$key]=0;
-            // }
         }
 
         $a['question_bloom']=$bloom->get();
