8853290644374d00617aff2eb4280fa00d3b6298
salma01 <salma.amr@learnovia.com>
2022-07-28 14:35:33 +0200

    notified in fees job

--- app/Jobs/FeesJob.php
+++ app/Jobs/FeesJob.php
@@ -39,20 +39,24 @@ class FeesJob implements ShouldQueue
      */
     public function handle()
     {
-        $reqNot=[
-            'message' => 'Fees is due by '.$this->installment->date ,
-            'item_id' => $this->installment->id,
-            'item_type' => 'fees',
-            'type' => 'notification',
-            'publish_date' => Carbon::now()->format('Y-m-d H:i:s'),
-        ];
-        $Installment_percentage = Installment::where('date' , '<=' , Carbon::parse($this->installment->date)->format('Y-m-d'))->sum('percentage');
-        $students = $this->chain->getEnrollsByManyChain(new Request())->select('user_id')->distinct('user_id')->where('role_id', 3)
-                    ->whereHas('user.fees',function($q) use ($Installment_percentage){  $q->where('percentage', '>', $Installment_percentage );  })->pluck('user_id');
+        if(Installment::count() > 0 && (bool) $this->installment->notified == false ){
+            $reqNot=[
+                'message' => 'Fees is due by '.$this->installment->date ,
+                'item_id' => $this->installment->id,
+                'item_type' => 'fees',
+                'type' => 'notification',
+                'publish_date' => Carbon::now()->format('Y-m-d H:i:s'),
+            ];
+            $Installment_percentage = Installment::where('date' , '<=' , Carbon::parse($this->installment->date)->format('Y-m-d'))->sum('percentage');
+            $students = $this->chain->getEnrollsByManyChain(new Request())->select('user_id')->distinct('user_id')->where('role_id', 3)
+                        ->whereHas('user.fees',function($q) use ($Installment_percentage){  $q->where('percentage', '>', $Installment_percentage );  })->pluck('user_id');
 
-        $users = Parents::select('parent_id')->distinct('parent_id')->whereIn('child_id', $students)->pluck('parent_id');
-        if($users->count() > 0)
-            $this->notification->sendNotify($users->toArray(),$reqNot);
-        
+            $users = Parents::select('parent_id')->distinct('parent_id')->whereIn('child_id', $students)->pluck('parent_id');
+            if($users->count() > 0)
+                $this->notification->sendNotify($users->toArray(),$reqNot);
+               
+            Installment::where('id',$this->installment->id)->update(['notified' => 1]);
+            
+        }
     }
 }
