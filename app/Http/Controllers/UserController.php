d8869287a605a24efa9ea8a6077ce0bbf32bee48
Hend_Kagha <hend.genady@learnovia.com>
2022-10-30 14:14:54 +0200

    unEnroll parent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -716,30 +716,11 @@ class UserController extends Controller
         ]);
 
         foreach($request->parent_id as $parent){
-
             foreach($request->child_id as $child){
-                
                 Parents::firstOrCreate([
                     'child_id' => $child,
                     'parent_id' => $parent
                 ]);
-
-                $students = Enroll::where('user_id',$child)->get();
-
-                foreach($students as $student){
-
-                    Enroll::firstOrCreate([
-                        // 'course_segment' => $student->course_segment,
-                        'user_id' => $parent,
-                        'role_id'=> 7,
-                        'year' => $student->year,
-                        'type' => $student->type,
-                        'level' => $student->level,
-                        'group' => $student->group,
-                        'segment' => $student->segment,
-                        'course' => $student->course
-                    ]);
-                }
             }
         }
         return HelperController::api_response_format(201,null,__('messages.users.parent_assign_child'));
