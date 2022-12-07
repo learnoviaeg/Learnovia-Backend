424d00523b623322118e224fe7c9b685a35251e6
Hend_Kagha <hend.genady@learnovia.com>
2022-12-07 11:22:35 +0200

    message of chat

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -708,41 +708,25 @@ class UserController extends Controller
     public function set_parent_child(Request $request)
     {
         $request->validate([
-            'parent_id' => 'required|array|exists:users,id',
-            'child_id' => 'required|array|exists:users,id'
+            'parent_id' => 'required|array',
+            'parent_id.*' => 'exists:users,id',
+            'child_id' => 'required|array',
+            'child_id.*' => 'exists:users,id'
         ]);
 
         foreach($request->parent_id as $parent){
-
             foreach($request->child_id as $child){
                 
                 Parents::firstOrCreate([
                     'child_id' => $child,
                     'parent_id' => $parent
                 ]);
-
-                $students = Enroll::where('user_id',$child)->get();
-
-                foreach($students as $student){
-
-                    Enroll::firstOrCreate([
-                        // 'course_segment' => $student->course_segment,
-                        'user_id' => $parent,
-                        'role_id'=> 7,
-                        'year' => $student->year,
-                        'type' => $student->type,
-                        'level' => $student->level,
-                        'group' => $student->group,
-                        'segment' => $student->segment,
-                        'course' => $student->course
-                    ]);
-                }
             }
         }
         return HelperController::api_response_format(201,null,__('messages.users.parent_assign_child'));
     }
 
-        /**
+    /**
      * set paresnt's child
      *
      * @return unAssigned Successfully
