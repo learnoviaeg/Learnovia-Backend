698c94d7535b748d01395f1f7800c2ca550471e4
Hend_Kagha <hend.genady@learnovia.com>
2022-12-22 15:07:28 +0200

    fix some issue

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -126,8 +126,8 @@ class ChatController extends Controller
     public function UploadFiles(Request $request)
     {
         $request->validate([
-            'file' => 'required|file|array',
-            'file.*' => 'distinct|mimes:pdf,docx,doc,xls,xlsx,ppt,pptx,zip,rar,jpeg,jpg,png,gif,mp4,avi,flv,wav,mpga,ogg,ogv,oga',
+            'file' => 'required|array',
+            'file.*' => 'file|distinct|mimes:pdf,docx,doc,xls,xlsx,ppt,pptx,zip,rar,jpeg,jpg,png,gif,mp4,avi,flv,wav,mpga,ogg,ogv,oga',
         ]);
 
         $attachment=[];
